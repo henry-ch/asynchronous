5b3dedafef2d0071e5af8454643a71b85ffe1267
henry-ch <christophe.j.henry@googlemail.com>
2014-11-27 10:24:34 +0100

    moved cutoff finding function to asynchronous namespace

--- libs/asynchronous/doc/examples/example_cutoff_sort.cpp
+++ libs/asynchronous/doc/examples/example_cutoff_sort.cpp
@@ -12,6 +12,9 @@
 #include <boost/asynchronous/checks.hpp>
 #include <boost/asynchronous/trackable_servant.hpp>
 #include <boost/asynchronous/algorithm/parallel_sort.hpp>
+// we want to see intermediate results
+#define BOOST_ASYNCHRONOUS_USE_COUT
+#include <boost/asynchronous/helpers.hpp>
 
 using namespace std;
 
@@ -24,58 +27,10 @@ void generate(std::vector<int>& test_data)
     std::uniform_int_distribution<> dis(0, 100000);
     std::generate(test_data.begin(), test_data.end(), std::bind(dis, std::ref(mt)));
 }
-
-// calls f(cutoff) on the given scheduler and measures elapsed time
-// returns elapsed time in us
-template <class Func, class Scheduler>
-std::size_t measure_cutoff(Scheduler s, Func f,std::size_t cutoff)
-{
-    typename boost::chrono::high_resolution_clock::time_point start;
-    typename boost::chrono::high_resolution_clock::time_point stop;
-    start = boost::chrono::high_resolution_clock::now();
-    auto fu = boost::asynchronous::post_future(s,[cutoff,&f]()mutable{return f(cutoff);});
-    fu.get();
-    stop = boost::chrono::high_resolution_clock::now();
-    return (boost::chrono::nanoseconds(stop - start).count() / 1000);
-}
-template <class Func, class Scheduler>
-std::tuple<std::size_t,std::vector<std::size_t>> find_best_cutoff(Scheduler s, Func f,
-                                                     std::size_t cutoff_min,
-                                                     std::size_t cutoff_max,
-                                                     std::size_t steps,
-                                                     std::size_t retries)
-{
-    std::map<std::size_t, std::vector<std::size_t>> map_cutoff_to_elapsed;
-    for (std::size_t i = 0; i< steps; ++i)
-    {
-        map_cutoff_to_elapsed[cutoff_min+((cutoff_max-cutoff_min)/steps)].reserve(retries);
-        for (std::size_t j = 0; j< retries; ++j)
-        {
-            std::size_t cutoff = cutoff_min+((double)(cutoff_max-cutoff_min)/steps)*i;
-            std::size_t one_step = measure_cutoff(s, f, cutoff);
-            map_cutoff_to_elapsed[cutoff].push_back(one_step);
-            std::cout << "example_cutoff_sort took for cutoff "<< cutoff << " in us:" << one_step << std::endl;
-        }
-    }
-    // look for best
-    std::size_t best_cutoff=0;
-    std::size_t best = std::numeric_limits<std::size_t>::max();
-    for (auto it = map_cutoff_to_elapsed.begin(); it != map_cutoff_to_elapsed.end();++it)
-    {
-        std::size_t acc = std::accumulate((*it).second.begin(),(*it).second.end(),0,[](std::size_t a, std::size_t b){return a+b;});
-        if (acc < best)
-        {
-            best_cutoff = (*it).first;
-            best = acc;
-        }
-    }
-    return std::make_tuple(best_cutoff,std::move(map_cutoff_to_elapsed[best_cutoff]));
 }
 
-}
-
-void example_cutoff_sort(std::size_t cutoff_min,
-                         std::size_t cutoff_max,
+void example_cutoff_sort(std::size_t cutoff_begin,
+                         std::size_t cutoff_end,
                          std::size_t steps,
                          std::size_t retries)
 {
@@ -92,10 +47,10 @@ void example_cutoff_sort(std::size_t cutoff_min,
         std::vector<int> test_data;
         generate(test_data);
         std::tuple<std::size_t,std::vector<std::size_t>> best_cutoff=
-                find_best_cutoff(scheduler,
+                boost::asynchronous::find_best_cutoff(scheduler,
                     [test_data](std::size_t cutoff)mutable
                     {return boost::asynchronous::parallel_sort_move(std::move(test_data),std::less<int>(),cutoff);},
-                    cutoff_min,cutoff_max,steps,retries
+                    cutoff_begin,cutoff_end,steps,retries
         );
         std::size_t acc = std::accumulate(std::get<1>(best_cutoff).begin(),std::get<1>(best_cutoff).end(),0,
                                           [](std::size_t a, std::size_t b){return a+b;});
