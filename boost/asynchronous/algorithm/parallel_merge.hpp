4420795a8c1ac6fe100a257dfe830c19a4d9a126
henry-ch <christophe.j.henry@googlemail.com>
2014-12-26 11:24:14 +0100

    added parallel_merge

---
+++ boost/asynchronous/algorithm/parallel_merge.hpp
@@ -0,0 +1,151 @@
+// Boost.Asynchronous library
+//  Copyright (C) Christophe Henry 2014
+//
+//  Use, modification and distribution is subject to the Boost
+//  Software License, Version 1.0.  (See accompanying file
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// For more information, see http://www.boost.org
+
+#ifndef BOOST_ASYNCHRONOUS_PARALLEL_MERGE_HPP
+#define BOOST_ASYNCHRONOUS_PARALLEL_MERGE_HPP
+
+#include <algorithm>
+
+#include <boost/utility/enable_if.hpp>
+#include <boost/asynchronous/callable_any.hpp>
+#include <boost/asynchronous/detail/continuation_impl.hpp>
+#include <boost/asynchronous/continuation_task.hpp>
+#include <boost/asynchronous/post.hpp>
+#include <boost/asynchronous/scheduler/serializable_task.hpp>
+#include <boost/asynchronous/algorithm/detail/safe_advance.hpp>
+#include <boost/asynchronous/detail/metafunctions.hpp>
+
+namespace boost { namespace asynchronous
+{
+namespace detail
+{
+template <class Iterator1, class Iterator2, class OutIterator, class Func, class Job>
+struct parallel_merge_helper: public boost::asynchronous::continuation_task<void>
+{
+    parallel_merge_helper(Iterator1 beg1, Iterator1 end1, Iterator2 beg2, Iterator2 end2, OutIterator out, Func func,long cutoff,
+                        const std::string& task_name, std::size_t prio)
+        : boost::asynchronous::continuation_task<void>(task_name),
+          beg1_(beg1),end1_(end1),beg2_(beg2),end2_(end2),out_(out),func_(std::move(func)),cutoff_(cutoff),prio_(prio)
+    {}
+    void operator()()const
+    {
+        boost::asynchronous::continuation_result<void> task_res = this_task_result();
+        auto length1 = std::distance(beg1_,end1_);
+        auto length2 = std::distance(beg2_,end2_);
+//        if (length1 == 0)
+//        {
+//            task_res.set_value();
+//            return;
+//        }
+        // if not at end, recurse, otherwise execute here
+        if ((length1+length2) <= cutoff_)
+        {
+            std::merge(beg1_,end1_,beg2_,end2_,out_,func_);
+            task_res.set_value();
+        }
+        else
+        {
+            if (length1 >= length2)
+            {
+                helper(beg1_,end1_,beg2_,end2_,out_,std::move(task_res));
+//                auto it1 = (beg1_+ (end1_ - beg1_)/2);
+//                auto it2 = std::lower_bound(beg2_,end2_, *it1);
+//                auto it3 = out_ + ((it1 - beg1_) + (it2 - beg2_));
+//                // TODO move?
+//                *it3 = *it1;
+//                boost::asynchronous::create_callback_continuation_job<Job>(
+//                            // called when subtasks are done, set our result
+//                            [task_res](std::tuple<boost::asynchronous::expected<void>,boost::asynchronous::expected<void> > res)
+//                            {
+//                                try
+//                                {
+//                                    // get to check that no exception
+//                                    std::get<0>(res).get();
+//                                    std::get<1>(res).get();
+//                                    task_res.set_value();
+//                                }
+//                                catch(std::exception& e)
+//                                {
+//                                    task_res.set_exception(boost::copy_exception(e));
+//                                }
+//                            },
+//                            // recursive tasks
+//                            parallel_merge_helper<Iterator1,Iterator2,OutIterator,Func,Job>
+//                                (beg1_,it1,beg2_,it2,out_,func_,cutoff_,this->get_name(),prio_),
+//                            parallel_merge_helper<Iterator1,Iterator2,OutIterator,Func,Job>
+//                                (it1+1,end1_,it2,end2_,it3+1,func_,cutoff_,this->get_name(),prio_)
+//                );
+            }
+            else
+            {
+                helper(beg2_,end2_,beg1_,end1_,out_,std::move(task_res));
+            }
+        }
+    }
+    template <class It1, class It2, class OutIt>
+    void helper(It1 beg1, It1 end1, It2 beg2, It2 end2, OutIt out,boost::asynchronous::continuation_result<void> task_res)const
+    {
+        auto it1 = (beg1+ (end1 - beg1)/2);
+        auto it2 = std::lower_bound(beg2,end2, *it1);
+        auto it3 = out + ((it1 - beg1) + (it2 - beg2));
+        // TODO move?
+        *it3 = *it1;
+        boost::asynchronous::create_callback_continuation_job<Job>(
+                    // called when subtasks are done, set our result
+                    [task_res](std::tuple<boost::asynchronous::expected<void>,boost::asynchronous::expected<void> > res)
+                    {
+                        try
+                        {
+                            // get to check that no exception
+                            std::get<0>(res).get();
+                            std::get<1>(res).get();
+                            task_res.set_value();
+                        }
+                        catch(std::exception& e)
+                        {
+                            task_res.set_exception(boost::copy_exception(e));
+                        }
+                    },
+                    // recursive tasks
+                    parallel_merge_helper<Iterator1,Iterator2,OutIterator,Func,Job>
+                        (beg1,it1,beg2,it2,out,func_,cutoff_,this->get_name(),prio_),
+                    parallel_merge_helper<Iterator1,Iterator2,OutIterator,Func,Job>
+                        (it1+1,end1,it2,end2,it3+1,func_,cutoff_,this->get_name(),prio_)
+        );
+    }
+
+    Iterator1 beg1_;
+    Iterator1 end1_;
+    Iterator2 beg2_;
+    Iterator2 end2_;
+    OutIterator out_;
+    Func func_;
+    long cutoff_;
+    std::size_t prio_;
+};
+}
+
+template <class Iterator1, class Iterator2, class OutIterator, class Func, class Job=BOOST_ASYNCHRONOUS_DEFAULT_JOB>
+// TODO out iterator instead of void
+boost::asynchronous::detail::callback_continuation<void,Job>
+parallel_merge(Iterator1 beg1, Iterator1 end1, Iterator2 beg2, Iterator2 end2, OutIterator out, Func func,long cutoff,
+#ifdef BOOST_ASYNCHRONOUS_REQUIRE_ALL_ARGUMENTS
+                    const std::string& task_name, std::size_t prio)
+#else
+                    const std::string& task_name="", std::size_t prio=0)
+#endif
+{
+    return boost::asynchronous::top_level_callback_continuation_job<void,Job>
+            (boost::asynchronous::detail::parallel_merge_helper<Iterator1,Iterator2,OutIterator,Func,Job>
+                (beg1,end1,beg2,end2,out,std::move(func),cutoff,task_name,prio));
+
+}
+
+}}
+#endif // BOOST_ASYNCHRONOUS_PARALLEL_MERGE_HPP
---
+++ libs/asynchronous/test/test_parallel_merge.cpp
@@ -0,0 +1,268 @@
+// Boost.Asynchronous library
+//  Copyright (C) Christophe Henry 2013
+//
+//  Use, modification and distribution is subject to the Boost
+//  Software License, Version 1.0.  (See accompanying file
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// For more information, see http://www.boost.org
+
+#include <vector>
+#include <set>
+#include <functional>
+#include <random>
+#include <boost/lexical_cast.hpp>
+
+#include <boost/asynchronous/scheduler/single_thread_scheduler.hpp>
+#include <boost/asynchronous/queue/lockfree_queue.hpp>
+#include <boost/asynchronous/scheduler_shared_proxy.hpp>
+#include <boost/asynchronous/scheduler/threadpool_scheduler.hpp>
+
+#include <boost/asynchronous/servant_proxy.hpp>
+#include <boost/asynchronous/post.hpp>
+#include <boost/asynchronous/trackable_servant.hpp>
+#include <boost/asynchronous/algorithm/parallel_merge.hpp>
+
+#include "test_common.hpp"
+
+#include <boost/test/unit_test.hpp>
+using namespace boost::asynchronous::test;
+
+namespace
+{
+// main thread id
+boost::thread::id main_thread_id;
+bool servant_dtor=false;
+
+struct my_exception : virtual boost::exception, virtual std::exception
+{
+    virtual const char* what() const throw()
+    {
+        return "my_exception";
+    }
+};
+void generate(std::vector<int>& data, unsigned elements, unsigned dist)
+{
+    data = std::vector<int>(elements,1);
+    std::mt19937 mt(static_cast<unsigned int>(std::time(nullptr)));
+    std::uniform_int_distribution<> dis(0, dist);
+    std::generate(data.begin(), data.end(), std::bind(dis, std::ref(mt)));
+}
+
+struct Servant : boost::asynchronous::trackable_servant<>
+{
+    typedef int simple_ctor;
+    Servant(boost::asynchronous::any_weak_scheduler<> scheduler)
+        : boost::asynchronous::trackable_servant<>(scheduler,
+                                               boost::asynchronous::create_shared_scheduler_proxy(
+                                                   new boost::asynchronous::threadpool_scheduler<
+                                                           boost::asynchronous::lockfree_queue<> >(boost::thread::hardware_concurrency())))
+    {
+    }
+    ~Servant()
+    {
+        BOOST_CHECK_MESSAGE(main_thread_id!=boost::this_thread::get_id(),"servant dtor not posted.");
+        servant_dtor = true;
+    }
+
+    boost::shared_future<void> start_parallel_merge_1000_700()
+    {
+        BOOST_CHECK_MESSAGE(main_thread_id!=boost::this_thread::get_id(),"servant async work not posted.");
+        generate(m_data1,1000,700);
+        generate(m_data2,1000,700);
+        std::sort(m_data1.begin(),m_data1.end(),std::less<int>());
+        std::sort(m_data2.begin(),m_data2.end(),std::less<int>());
+        m_res.resize(2000);
+        // we need a promise to inform caller when we're done
+        boost::shared_ptr<boost::promise<void> > aPromise(new boost::promise<void>);
+        boost::shared_future<void> fu = aPromise->get_future();
+        boost::asynchronous::any_shared_scheduler_proxy<> tp =get_worker();
+        auto data_copy1 = m_data1;
+        auto data_copy2 = m_data2;
+        // start long tasks
+        post_callback(
+           [tp,this](){
+                    BOOST_CHECK_MESSAGE(main_thread_id!=boost::this_thread::get_id(),"servant work not posted.");
+                    std::vector<boost::thread::id> ids = tp.thread_ids();
+                    BOOST_CHECK_MESSAGE(contains_id(ids.begin(),ids.end(),boost::this_thread::get_id()),"task executed in the wrong thread");
+                    return boost::asynchronous::parallel_merge(m_data1.begin(),m_data1.end(),
+                                                               m_data2.begin(),m_data2.end(),
+                                                               m_res.begin(),
+                                                               std::less<int>(),100);
+                    },// work
+           [aPromise,tp,data_copy1,data_copy2,this](boost::asynchronous::expected<void> res) mutable{
+                        BOOST_CHECK_MESSAGE(!res.has_exception(),"servant work threw an exception.");
+                        BOOST_CHECK_MESSAGE(main_thread_id!=boost::this_thread::get_id(),"servant callback in main thread.");
+                        std::vector<boost::thread::id> ids = tp.thread_ids();
+                        BOOST_CHECK_MESSAGE(!contains_id(ids.begin(),ids.end(),boost::this_thread::get_id()),"task callback executed in the wrong thread(pool)");
+                        BOOST_CHECK_MESSAGE(!res.has_exception(),"servant work threw an exception.");
+                        std::vector<int> merged;
+                        merged.resize(2000);
+                        std::merge(data_copy1.begin(),data_copy1.end(),data_copy2.begin(),data_copy2.end(),merged.begin(),std::less<int>());
+                        BOOST_CHECK_MESSAGE(merged == m_res,"parallel_merge gave a wrong value.");
+                        aPromise->set_value();
+           }// callback functor.
+        );
+        return fu;
+    }
+    boost::shared_future<void> start_parallel_merge_1000_1000()
+    {
+        BOOST_CHECK_MESSAGE(main_thread_id!=boost::this_thread::get_id(),"servant async work not posted.");
+        generate(m_data1,1000,1000);
+        generate(m_data2,1000,1000);
+        std::sort(m_data1.begin(),m_data1.end(),std::less<int>());
+        std::sort(m_data2.begin(),m_data2.end(),std::less<int>());
+        m_res.resize(2000);
+        // we need a promise to inform caller when we're done
+        boost::shared_ptr<boost::promise<void> > aPromise(new boost::promise<void>);
+        boost::shared_future<void> fu = aPromise->get_future();
+        boost::asynchronous::any_shared_scheduler_proxy<> tp =get_worker();
+        auto data_copy1 = m_data1;
+        auto data_copy2 = m_data2;
+        // start long tasks
+        post_callback(
+           [tp,this](){
+                    BOOST_CHECK_MESSAGE(main_thread_id!=boost::this_thread::get_id(),"servant work not posted.");
+                    std::vector<boost::thread::id> ids = tp.thread_ids();
+                    BOOST_CHECK_MESSAGE(contains_id(ids.begin(),ids.end(),boost::this_thread::get_id()),"task executed in the wrong thread");
+                    return boost::asynchronous::parallel_merge(m_data1.begin(),m_data1.end(),
+                                                               m_data2.begin(),m_data2.end(),
+                                                               m_res.begin(),
+                                                               std::less<int>(),100);
+                    },// work
+           [aPromise,tp,data_copy1,data_copy2,this](boost::asynchronous::expected<void> res) mutable{
+                        BOOST_CHECK_MESSAGE(!res.has_exception(),"servant work threw an exception.");
+                        BOOST_CHECK_MESSAGE(main_thread_id!=boost::this_thread::get_id(),"servant callback in main thread.");
+                        std::vector<boost::thread::id> ids = tp.thread_ids();
+                        BOOST_CHECK_MESSAGE(!contains_id(ids.begin(),ids.end(),boost::this_thread::get_id()),"task callback executed in the wrong thread(pool)");
+                        BOOST_CHECK_MESSAGE(!res.has_exception(),"servant work threw an exception.");
+                        std::vector<int> merged;
+                        merged.resize(2000);
+                        std::merge(data_copy1.begin(),data_copy1.end(),data_copy2.begin(),data_copy2.end(),merged.begin(),std::less<int>());
+                        BOOST_CHECK_MESSAGE(merged == m_res,"parallel_merge gave a wrong value.");
+                        aPromise->set_value();
+           }// callback functor.
+        );
+        return fu;
+    }
+    boost::shared_future<void> start_parallel_merge_100000000_80000000()
+    {
+        BOOST_CHECK_MESSAGE(main_thread_id!=boost::this_thread::get_id(),"servant async work not posted.");
+        generate(m_data1,100000000,80000000);
+        generate(m_data2,100000000,80000000);
+        std::sort(m_data1.begin(),m_data1.end(),std::less<int>());
+        std::sort(m_data2.begin(),m_data2.end(),std::less<int>());
+        m_res.resize(200000000);
+        // we need a promise to inform caller when we're done
+        boost::shared_ptr<boost::promise<void> > aPromise(new boost::promise<void>);
+        boost::shared_future<void> fu = aPromise->get_future();
+        // single-threaded
+        std::vector<int> merged;
+        merged.resize(200000000);
+        auto start = boost::chrono::high_resolution_clock::now();
+        std::merge(m_data1.begin(),m_data1.end(),m_data2.begin(),m_data2.end(),merged.begin(),std::less<int>());
+        double elapsed = (boost::chrono::nanoseconds(boost::chrono::high_resolution_clock::now() - start).count() / 1000000);
+        std::cout << "single-threaded merge took in ms: " << elapsed << std::endl;
+        // parallel
+        start = boost::chrono::high_resolution_clock::now();
+        // start long tasks
+        post_callback(
+           [this](){
+                    return boost::asynchronous::parallel_merge(m_data1.begin(),m_data1.end(),
+                                                               m_data2.begin(),m_data2.end(),
+                                                               m_res.begin(),
+                                                               std::less<int>(),10000000);
+                    },// work
+           [aPromise,start,this](boost::asynchronous::expected<void>) mutable{
+                        double elapsed = (boost::chrono::nanoseconds(boost::chrono::high_resolution_clock::now() - start).count() / 1000000);
+                        aPromise->set_value();
+                        std::cout << "multi-threaded merge took in ms: " << elapsed << std::endl;
+           }// callback functor.
+        );
+        return fu;
+    }
+private:
+    std::vector<int> m_data1;
+    std::vector<int> m_data2;
+    std::vector<int> m_res;
+};
+class ServantProxy : public boost::asynchronous::servant_proxy<ServantProxy,Servant>
+{
+public:
+    template <class Scheduler>
+    ServantProxy(Scheduler s):
+        boost::asynchronous::servant_proxy<ServantProxy,Servant>(s)
+    {}
+    BOOST_ASYNC_FUTURE_MEMBER(start_parallel_merge_1000_700)
+    BOOST_ASYNC_FUTURE_MEMBER(start_parallel_merge_1000_1000)
+    BOOST_ASYNC_FUTURE_MEMBER(start_parallel_merge_100000000_80000000)
+};
+}
+
+BOOST_AUTO_TEST_CASE( test_parallel_merge_1000_700 )
+{
+    servant_dtor=false;
+    {
+        auto scheduler = boost::asynchronous::create_shared_scheduler_proxy(new boost::asynchronous::single_thread_scheduler<
+                                                                            boost::asynchronous::lockfree_queue<> >);
+
+        main_thread_id = boost::this_thread::get_id();
+        ServantProxy proxy(scheduler);
+        boost::shared_future<boost::shared_future<void> > fuv = proxy.start_parallel_merge_1000_700();
+        try
+        {
+            boost::shared_future<void> resfuv = fuv.get();
+            resfuv.get();
+        }
+        catch(...)
+        {
+            BOOST_FAIL( "unexpected exception" );
+        }
+    }
+    BOOST_CHECK_MESSAGE(servant_dtor,"servant dtor not called.");
+}
+BOOST_AUTO_TEST_CASE( test_parallel_merge_1000_1000 )
+{
+    servant_dtor=false;
+    {
+        auto scheduler = boost::asynchronous::create_shared_scheduler_proxy(new boost::asynchronous::single_thread_scheduler<
+                                                                            boost::asynchronous::lockfree_queue<> >);
+
+        main_thread_id = boost::this_thread::get_id();
+        ServantProxy proxy(scheduler);
+        boost::shared_future<boost::shared_future<void> > fuv = proxy.start_parallel_merge_1000_1000();
+        try
+        {
+            boost::shared_future<void> resfuv = fuv.get();
+            resfuv.get();
+        }
+        catch(...)
+        {
+            BOOST_FAIL( "unexpected exception" );
+        }
+    }
+    BOOST_CHECK_MESSAGE(servant_dtor,"servant dtor not called.");
+}
+// for performance tests
+//BOOST_AUTO_TEST_CASE( test_parallel_merge_100000000_80000000 )
+//{
+//    servant_dtor=false;
+//    {
+//        auto scheduler = boost::asynchronous::create_shared_scheduler_proxy(new boost::asynchronous::single_thread_scheduler<
+//                                                                            boost::asynchronous::lockfree_queue<> >);
+
+//        main_thread_id = boost::this_thread::get_id();
+//        ServantProxy proxy(scheduler);
+//        boost::shared_future<boost::shared_future<void> > fuv = proxy.start_parallel_merge_100000000_80000000();
+//        try
+//        {
+//            boost::shared_future<void> resfuv = fuv.get();
+//            resfuv.get();
+//        }
+//        catch(...)
+//        {
+//            BOOST_FAIL( "unexpected exception" );
+//        }
+//    }
+//    BOOST_CHECK_MESSAGE(servant_dtor,"servant dtor not called.");
+//}
