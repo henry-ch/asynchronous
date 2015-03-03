650e9a3bd4e26126fe31930c78f14577593d2ae0
henry-ch <christophe.j.henry@googlemail.com>
2015-03-03 21:57:36 +0100

    added test_geometry_union_of_x

---
+++ libs/asynchronous/test/sinus_buffer.hpp
@@ -0,0 +1,170 @@
+// Boost.Geometry (aka GGL, Generic Geometry Library)
+
+// Copyright (c) 2014 Barend Gehrels, Amsterdam, the Netherlands.
+
+// Use, modification and distribution is subject to the Boost Software License,
+// Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef BOOST_GEOMETRY_SINUS_BUFFER_HPP
+#define BOOST_GEOMETRY_SINUS_BUFFER_HPP
+
+//static double M_PI = 3.14159265;
+
+// Function to let buffer-distance depend on alpha, e.g.:
+inline double corrected_distance(double distance, double alpha)
+{
+    return distance * 1.0 + 0.2 * sin(alpha * 6.0);
+}
+
+class buffer_point_strategy_sample
+{
+public :
+
+    template
+    <
+        typename Point,
+        typename OutputRange,
+        typename DistanceStrategy
+    >
+    void apply(Point const& point,
+            DistanceStrategy const& distance_strategy,
+            OutputRange& output_range) const
+    {
+        namespace bg = boost::geometry;
+        const int point_count = 90; // points for a full circle
+
+        double const distance = distance_strategy.apply(point, point,
+                        bg::strategy::buffer::buffer_side_left);
+
+        double const angle_increment = 2.0 * M_PI / double(point_count);
+
+        double alpha = 0;
+        for (std::size_t i = 0; i <= point_count; i++, alpha -= angle_increment)
+        {
+            double const cd = corrected_distance(distance, alpha);
+
+            typename boost::range_value<OutputRange>::type output_point;
+            bg::set<0>(output_point, bg::get<0>(point) + cd * cos(alpha));
+            bg::set<1>(output_point, bg::get<1>(point) + cd * sin(alpha));
+            output_range.push_back(output_point);
+        }
+    }
+};
+
+class buffer_join_strategy_sample
+{
+private :
+    template
+    <
+        typename Point,
+        typename DistanceType,
+        typename RangeOut
+    >
+    inline void generate_points(Point const& vertex,
+                Point const& perp1, Point const& perp2,
+                DistanceType const& buffer_distance,
+                RangeOut& range_out) const
+    {
+        namespace bg = boost::geometry;
+        const int point_count = 90; // points for a full circle
+
+        double dx1 = bg::get<0>(perp1) - bg::get<0>(vertex);
+        double dy1 = bg::get<1>(perp1) - bg::get<1>(vertex);
+        double dx2 = bg::get<0>(perp2) - bg::get<0>(vertex);
+        double dy2 = bg::get<1>(perp2) - bg::get<1>(vertex);
+
+        // Assuming the corner is convex, angle2 < angle1
+        double const angle1 = atan2(dy1, dx1);
+        double angle2 = atan2(dy2, dx2);
+
+        while (angle2 > angle1)
+        {
+            angle2 -= 2 * M_PI;
+        }
+
+        double const angle_increment = 2.0 * M_PI / double(point_count);
+        double alpha = angle1 - angle_increment;
+
+        for (int i = 0; alpha >= angle2 && i < point_count; i++, alpha -= angle_increment)
+        {
+            double cd = corrected_distance(buffer_distance, alpha);
+
+            Point p;
+            bg::set<0>(p, bg::get<0>(vertex) + cd * cos(alpha));
+            bg::set<1>(p, bg::get<1>(vertex) + cd * sin(alpha));
+            range_out.push_back(p);
+        }
+    }
+
+public :
+
+    template <typename Point, typename DistanceType, typename RangeOut>
+    inline bool apply(Point const& ip, Point const& vertex,
+                Point const& perp1, Point const& perp2,
+                DistanceType const& buffer_distance,
+                RangeOut& range_out) const
+    {
+        generate_points(vertex, perp1, perp2, buffer_distance, range_out);
+        return true;
+    }
+
+    template <typename NumericType>
+    static inline NumericType max_distance(NumericType const& distance)
+    {
+        return distance;
+    }
+
+};
+
+class buffer_side_sample
+{
+public :
+    template
+    <
+        typename Point,
+        typename OutputRange,
+        typename DistanceStrategy
+    >
+    static inline void apply(
+                Point const& input_p1, Point const& input_p2,
+                boost::geometry::strategy::buffer::buffer_side_selector side,
+                DistanceStrategy const& distance,
+                OutputRange& output_range)
+    {
+        namespace bg = boost::geometry;
+        // Generate a block along (left or right of) the segment
+
+        double const dx = bg::get<0>(input_p2) - bg::get<0>(input_p1);
+        double const dy = bg::get<1>(input_p2) - bg::get<1>(input_p1);
+
+        // For normalization [0,1] (=dot product d.d, sqrt)
+        double const length = bg::math::sqrt(dx * dx + dy * dy);
+
+        if (bg::math::equals(length, 0))
+        {
+            return;
+        }
+
+        // Generate the normalized perpendicular p, to the left (ccw)
+        double const px = -dy / length;
+        double const py = dx / length;
+
+        // Both vectors perpendicular to input p1 and input p2 have same angle
+        double const alpha = atan2(py, px);
+
+        double const d = distance.apply(input_p1, input_p2, side);
+
+        double const cd = corrected_distance(d, alpha);
+
+        output_range.resize(2);
+
+        bg::set<0>(output_range.front(), bg::get<0>(input_p1) + px * cd);
+        bg::set<1>(output_range.front(), bg::get<1>(input_p1) + py * cd);
+        bg::set<0>(output_range.back(), bg::get<0>(input_p2) + px * cd);
+        bg::set<1>(output_range.back(), bg::get<1>(input_p2) + py * cd);
+    }
+};
+
+#endif // BOOST_GEOMETRY_SINUS_BUFFER_HPP
+
---
+++ libs/asynchronous/test/test_geometry_union_of_x.cpp
@@ -0,0 +1,111 @@
+// Boost.Asynchronous library
+//  Copyright (C) Christophe Henry 2015
+//
+//  Use, modification and distribution is subject to the Boost
+//  Software License, Version 1.0.  (See accompanying file
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// For more information, see http://www.boost.org
+
+#include <vector>
+#include <set>
+#include <functional>
+#include <random>
+
+#include <boost/asynchronous/scheduler/single_thread_scheduler.hpp>
+#include <boost/asynchronous/queue/lockfree_queue.hpp>
+#include <boost/asynchronous/scheduler_shared_proxy.hpp>
+#include <boost/asynchronous/scheduler/multiqueue_threadpool_scheduler.hpp>
+
+#include <boost/geometry.hpp>
+#include <boost/geometry/multi/geometries/multi_geometries.hpp>
+
+#include <boost/random/linear_congruential.hpp>
+#include <boost/random/uniform_int.hpp>
+#include <boost/random/uniform_real.hpp>
+#include <boost/random/variate_generator.hpp>
+
+
+#include <boost/asynchronous/algorithm/geometry/parallel_geometry_union_of_x.hpp>
+
+
+#include "test_common.hpp"
+#include "sinus_buffer.hpp"
+
+#include <boost/test/unit_test.hpp>
+using namespace boost::asynchronous::test;
+namespace bg = boost::geometry;
+
+
+BOOST_AUTO_TEST_CASE( test_geometry_union_of_x_200_200_2 )
+{
+    typedef bg::model::point<double, 2, bg::cs::cartesian> point;
+    typedef bg::model::polygon<point> polygon_type;
+    typedef bg::model::multi_polygon<polygon_type> multi_polygon_type;
+
+    // Declare main object: a vector of (multi) polygons which will be unioned
+    std::vector<multi_polygon_type> many_polygons;
+
+    // Predefined strategies
+    bg::strategy::buffer::distance_symmetric<double> distance_strategy(2.0);
+    bg::strategy::buffer::end_flat end_strategy; // not effectively used
+
+    // Own strategies
+    buffer_join_strategy_sample join_strategy;
+    buffer_point_strategy_sample point_strategy;
+    buffer_side_sample side_strategy;
+
+    // Use a bit of random disturbance in the to be generated grid
+    typedef boost::minstd_rand base_generator_type;
+    base_generator_type generator(12345);
+    boost::uniform_real<> random_range(0.0, 0.5);
+    boost::variate_generator
+    <
+        base_generator_type&,
+        boost::uniform_real<>
+    > random(generator, random_range);
+
+    for (int i = 0; i < 200; i++)
+    {
+        for (int j = 0; j < 200; j++)
+        {
+            double x = i * 3.0 + random();
+            double y = j * 3.0 + random();
+
+            point p(x, y);
+
+            multi_polygon_type buffered;
+            // Create the buffer of a point
+            bg::buffer(p, buffered,
+                        distance_strategy, side_strategy,
+                        join_strategy, end_strategy, point_strategy);
+
+            many_polygons.push_back(buffered);
+        }
+    }
+
+    auto scheduler = boost::asynchronous::create_shared_scheduler_proxy(new boost::asynchronous::multiqueue_threadpool_scheduler<
+                                                                                boost::asynchronous::lockfree_queue<> >(
+                                                                                    boost::thread::hardware_concurrency()));
+
+    // make a copy and execute in pool
+    boost::future<std::vector<multi_polygon_type>> fu = boost::asynchronous::post_future(scheduler,
+    [many_polygons=std::move(many_polygons)]()mutable
+    {
+        return boost::asynchronous::parallel_geometry_union_of_x<std::vector<multi_polygon_type>,
+                                                                 BOOST_ASYNCHRONOUS_DEFAULT_JOB>
+                (std::move(many_polygons),"",0);
+    }
+    ,"",0);
+
+    try
+    {
+        std::vector<multi_polygon_type> final_output = std::move(fu.get());
+        BOOST_CHECK_MESSAGE((long)bg::area(final_output.front()) == (long)358564,"parallel_geometry_union_of_x gave a wrong value.");
+    }
+    catch(...)
+    {
+        BOOST_FAIL( "unexpected exception" );
+    }
+}
+
