c615382ebbeeb53ced7ec5ffddfcd7cd9bb6ad45
henry-ch <christophe.j.henry@googlemail.com>
2015-03-17 07:30:46 +0100

    added if/else for continuations (else_if still missing)

---
+++ boost/asynchronous/algorithm/if_else.hpp
@@ -0,0 +1,157 @@
+// Boost.Asynchronous library
+//  Copyright (C) Christophe Henry 2013
+//
+//  Use, modification and distribution is subject to the Boost
+//  Software License, Version 1.0.  (See accompanying file
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// For more information, see http://www.boost.org
+
+#ifndef BOOST_ASYNCHRONOUS_IF_CONTINUATION_HPP
+#define BOOST_ASYNCHRONOUS_IF_CONTINUATION_HPP
+
+#include <boost/asynchronous/detail/continuation_impl.hpp>
+#include <boost/asynchronous/continuation_task.hpp>
+
+namespace boost { namespace asynchronous
+{
+namespace detail
+{
+template <class Continuation, class IfClause, class ThenClause>
+struct if_continuation_helper :
+        public boost::asynchronous::continuation_task<std::tuple<bool,typename Continuation::return_type>>
+{
+    if_continuation_helper(Continuation const& c,IfClause if_clause,ThenClause then_clause,
+                   const std::string& task_name)
+        :boost::asynchronous::continuation_task<std::tuple<bool,typename Continuation::return_type>>(task_name)
+        ,cont_(c)
+        ,if_clause_(if_clause)
+        ,then_clause_(then_clause)
+    {}
+    void operator()()
+    {
+        boost::asynchronous::continuation_result<std::tuple<bool,typename Continuation::return_type>> task_res =
+                this->this_task_result();
+        auto if_clause = if_clause_;
+        auto then_clause = then_clause_;
+        // TODO C++14 move capture if possible
+        cont_.on_done(
+        [task_res,if_clause,then_clause]
+        (std::tuple<boost::asynchronous::expected<typename Continuation::return_type> >&& continuation_res)
+        {
+            try
+            {
+                auto res = std::get<0>(continuation_res).get();
+                if (if_clause(res))
+                {
+                    auto then_cont = then_clause(std::move(res));
+                    then_cont.on_done(
+                        [task_res](std::tuple<boost::asynchronous::expected<typename Continuation::return_type> >&& then_res)
+                        {
+                            task_res.set_value(std::make_tuple(true,std::move(std::get<0>(then_res).get())));
+                        }
+                    );
+                }
+                else
+                {
+                    task_res.set_value(std::make_tuple(false,std::move(res)));
+                }
+            }
+            catch(std::exception& e)
+            {
+                task_res.set_exception(boost::copy_exception(e));
+            }
+        }
+        );
+    }
+    Continuation cont_;
+    IfClause if_clause_;
+    ThenClause then_clause_;
+};
+
+template <class Continuation, class ElseClause>
+struct else_continuation_helper :
+        public boost::asynchronous::continuation_task<typename std::tuple_element<1,typename Continuation::return_type>::type>
+{
+    else_continuation_helper(Continuation const& c,ElseClause else_clause,
+                   const std::string& task_name)
+        :boost::asynchronous::continuation_task<typename std::tuple_element<1,typename Continuation::return_type>::type>(task_name)
+        ,cont_(c)
+        ,else_clause_(else_clause)
+    {}
+    void operator()()
+    {
+        boost::asynchronous::continuation_result<typename std::tuple_element<1,typename Continuation::return_type>::type>
+                task_res = this->this_task_result();
+        auto else_clause = else_clause_;
+        // TODO C++14 move capture if possible
+        cont_.on_done(
+        [task_res,else_clause]
+        (std::tuple<boost::asynchronous::expected<typename Continuation::return_type> >&& continuation_res)
+        {
+            try
+            {
+                auto res = std::get<0>(continuation_res).get();
+                if (std::get<0>(res))
+                {
+                    // if handled, just forward
+                    task_res.set_value(std::move(std::get<1>(res)));
+                }
+                else
+                {
+                    auto else_cond = else_clause(std::move(std::get<1>(res)));
+                    else_cond.on_done(
+                        [task_res](std::tuple<boost::asynchronous::expected<
+                                            typename std::tuple_element<1,typename Continuation::return_type>::type> >&& prev_res)
+                        {
+                            task_res.set_value(std::move(std::get<0>(prev_res).get()));
+                        }
+                    );
+                }
+            }
+            catch(std::exception& e)
+            {
+                task_res.set_exception(boost::copy_exception(e));
+            }
+        }
+        );
+    }
+    Continuation cont_;
+    ElseClause else_clause_;
+};
+}
+
+template <class Continuation, class IfClause, class ThenClause,class Job=BOOST_ASYNCHRONOUS_DEFAULT_JOB>
+boost::asynchronous::detail::callback_continuation<std::tuple<bool,typename Continuation::return_type>,Job>
+if_(Continuation cont,IfClause if_clause, ThenClause then_clause,
+#ifdef BOOST_ASYNCHRONOUS_REQUIRE_ALL_ARGUMENTS
+             const std::string& task_name)
+#else
+             const std::string& task_name="")
+#endif
+{
+
+    return boost::asynchronous::top_level_callback_continuation_job<std::tuple<bool,typename Continuation::return_type>,Job>
+            (boost::asynchronous::detail::if_continuation_helper<Continuation,IfClause,ThenClause>
+                (std::move(cont),std::move(if_clause),std::move(then_clause),task_name));
+}
+
+template <class Continuation, class ElseClause,class Job=BOOST_ASYNCHRONOUS_DEFAULT_JOB>
+boost::asynchronous::detail::callback_continuation<typename std::tuple_element<1,typename Continuation::return_type>::type,Job>
+else_(Continuation cont,ElseClause else_clause,
+#ifdef BOOST_ASYNCHRONOUS_REQUIRE_ALL_ARGUMENTS
+             const std::string& task_name)
+#else
+             const std::string& task_name="")
+#endif
+{
+
+    return boost::asynchronous::top_level_callback_continuation_job<
+            typename std::tuple_element<1,typename Continuation::return_type>::type,Job>
+                (boost::asynchronous::detail::else_continuation_helper<Continuation,ElseClause>
+                    (std::move(cont),std::move(else_clause),task_name));
+}
+}}
+
+#endif // BOOST_ASYNCHRONOUS_IF_CONTINUATION_HPP
+
---
+++ libs/asynchronous/test/test_if_else.cpp
@@ -0,0 +1,271 @@
+
+
+// Boost.Asynchronous library
+//  Copyright (C) Christophe Henry 2013
+//
+//  Use, modification and distribution is subject to the Boost
+//  Software License, Version 1.0.  (See accompanying file
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// For more information, see http://www.boost.org
+
+#include <vector>
+#include <set>
+
+#include <boost/asynchronous/scheduler/single_thread_scheduler.hpp>
+#include <boost/asynchronous/queue/lockfree_queue.hpp>
+#include <boost/asynchronous/scheduler_shared_proxy.hpp>
+#include <boost/asynchronous/scheduler/threadpool_scheduler.hpp>
+
+#include <boost/asynchronous/servant_proxy.hpp>
+#include <boost/asynchronous/post.hpp>
+#include <boost/asynchronous/trackable_servant.hpp>
+#include <boost/asynchronous/algorithm/parallel_for.hpp>
+#include <boost/asynchronous/algorithm/if_else.hpp>
+
+#include "test_common.hpp"
+
+#include <boost/test/unit_test.hpp>
+using namespace boost::asynchronous::test;
+
+namespace
+{
+// main thread id
+boost::thread::id main_thread_id;
+bool servant_dtor=false;
+
+struct Servant : boost::asynchronous::trackable_servant<>
+{
+    typedef int simple_ctor;
+    Servant(boost::asynchronous::any_weak_scheduler<> scheduler)
+        : boost::asynchronous::trackable_servant<>(scheduler,
+                                               boost::asynchronous::create_shared_scheduler_proxy(
+                                                   new boost::asynchronous::threadpool_scheduler<
+                                                           boost::asynchronous::lockfree_queue<> >(6)))
+        , m_data(10000,1)
+    {
+    }
+    ~Servant()
+    {
+        BOOST_CHECK_MESSAGE(main_thread_id!=boost::this_thread::get_id(),"servant dtor not posted.");
+        servant_dtor = true;
+    }
+
+    boost::shared_future<void> async_if_true()
+    {
+        BOOST_CHECK_MESSAGE(main_thread_id!=boost::this_thread::get_id(),"servant start_async_work_range3 not posted.");
+        // we need a promise to inform caller when we're done
+        boost::shared_ptr<boost::promise<void> > aPromise(new boost::promise<void>);
+        boost::shared_future<void> fu = aPromise->get_future();
+        boost::asynchronous::any_shared_scheduler_proxy<> tp =get_worker();
+        // start long tasks
+        post_callback(
+           [tp,this](){
+                    BOOST_CHECK_MESSAGE(main_thread_id!=boost::this_thread::get_id(),"servant work not posted.");
+                    std::vector<boost::thread::id> ids = tp.thread_ids();
+                    BOOST_CHECK_MESSAGE(contains_id(ids.begin(),ids.end(),boost::this_thread::get_id()),"task executed in the wrong thread");
+                    return boost::asynchronous::parallel_for(
+                                boost::asynchronous::else_(
+                                    boost::asynchronous::if_(
+                                        boost::asynchronous::parallel_for(
+                                                                         std::move(this->m_data),
+                                                                         [](int const& i)
+                                                                         {
+                                                                            const_cast<int&>(i) += 2;
+                                                                         },1500),
+                                        [](std::vector<int> const&){return true;},
+                                        [](std::vector<int> res)
+                                        {
+                                            return boost::asynchronous::parallel_for(
+                                                                std::move(res),
+                                                                [](int const& i)
+                                                                {
+                                                                   const_cast<int&>(i) += 3;
+                                                                },1500);
+                                        }
+                                    ),
+                                    [](std::vector<int> res)
+                                    {
+                                        return boost::asynchronous::parallel_for(
+                                                            std::move(res),
+                                                            [](int const& i)
+                                                            {
+                                                               const_cast<int&>(i) += 4;
+                                                            },1500);
+                                    }
+                              ),
+                        [](int const& i)
+                        {
+                           const_cast<int&>(i) += 1;
+                        },1500
+                    );
+                    },// work
+           [aPromise,tp,this](boost::asynchronous::expected<std::vector<int>> res){
+                        BOOST_CHECK_MESSAGE(!res.has_exception(),"servant work threw an exception.");
+                        BOOST_CHECK_MESSAGE(main_thread_id!=boost::this_thread::get_id(),"servant callback in main thread.");
+                        std::vector<boost::thread::id> ids = tp.thread_ids();
+                        BOOST_CHECK_MESSAGE(!contains_id(ids.begin(),ids.end(),boost::this_thread::get_id()),"task callback executed in the wrong thread(pool)");
+                        BOOST_CHECK_MESSAGE(!res.has_exception(),"servant work threw an exception.");
+                        auto modified_vec = res.get();
+                        auto it = modified_vec.begin();
+                        BOOST_CHECK_MESSAGE(*it == 7,"data[0] is wrong: "+ std::to_string(*it));
+                        std::advance(it,100);
+                        BOOST_CHECK_MESSAGE(*it == 7,"data[100] is wrong: "+ std::to_string(*it));
+                        std::advance(it,900);
+                        BOOST_CHECK_MESSAGE(*it == 7,"data[1000] is wrong: "+ std::to_string(*it));
+                        std::advance(it,8999);
+                        BOOST_CHECK_MESSAGE(*it == 7,"data[9999] is wrong: "+ std::to_string(*it));
+                        auto r = std::accumulate(modified_vec.begin(),modified_vec.end(),0,[](int a, int b){return a+b;});
+                        BOOST_CHECK_MESSAGE((r == 70000),
+                                            ("result of parallel_for after if/else was " + std::to_string(r) +
+                                             ", should have been 70000"));
+
+                        // reset
+                        m_data = std::vector<int>(10000,1);
+                        aPromise->set_value();
+           }// callback functor.
+        );
+        return fu;
+    }
+
+    boost::shared_future<void> async_if_false()
+    {
+        BOOST_CHECK_MESSAGE(main_thread_id!=boost::this_thread::get_id(),"servant start_async_work_range3 not posted.");
+        // we need a promise to inform caller when we're done
+        boost::shared_ptr<boost::promise<void> > aPromise(new boost::promise<void>);
+        boost::shared_future<void> fu = aPromise->get_future();
+        boost::asynchronous::any_shared_scheduler_proxy<> tp =get_worker();
+        // start long tasks
+        post_callback(
+           [tp,this](){
+                    BOOST_CHECK_MESSAGE(main_thread_id!=boost::this_thread::get_id(),"servant work not posted.");
+                    std::vector<boost::thread::id> ids = tp.thread_ids();
+                    BOOST_CHECK_MESSAGE(contains_id(ids.begin(),ids.end(),boost::this_thread::get_id()),"task executed in the wrong thread");
+                    return boost::asynchronous::parallel_for(
+                                boost::asynchronous::else_(
+                                    boost::asynchronous::if_(
+                                        boost::asynchronous::parallel_for(
+                                                                         std::move(this->m_data),
+                                                                         [](int const& i)
+                                                                         {
+                                                                            const_cast<int&>(i) += 2;
+                                                                         },1500),
+                                        [](std::vector<int> const&){return false;},
+                                        [](std::vector<int> res)
+                                        {
+                                            return boost::asynchronous::parallel_for(
+                                                                std::move(res),
+                                                                [](int const& i)
+                                                                {
+                                                                   const_cast<int&>(i) += 3;
+                                                                },1500);
+                                        }
+                                    ),
+                                    [](std::vector<int> res)
+                                    {
+                                        return boost::asynchronous::parallel_for(
+                                                            std::move(res),
+                                                            [](int const& i)
+                                                            {
+                                                               const_cast<int&>(i) += 4;
+                                                            },1500);
+                                    }
+                              ),
+                        [](int const& i)
+                        {
+                           const_cast<int&>(i) += 1;
+                        },1500
+                    );
+                    },// work
+           [aPromise,tp,this](boost::asynchronous::expected<std::vector<int>> res){
+                        BOOST_CHECK_MESSAGE(!res.has_exception(),"servant work threw an exception.");
+                        BOOST_CHECK_MESSAGE(main_thread_id!=boost::this_thread::get_id(),"servant callback in main thread.");
+                        std::vector<boost::thread::id> ids = tp.thread_ids();
+                        BOOST_CHECK_MESSAGE(!contains_id(ids.begin(),ids.end(),boost::this_thread::get_id()),"task callback executed in the wrong thread(pool)");
+                        BOOST_CHECK_MESSAGE(!res.has_exception(),"servant work threw an exception.");
+                        auto modified_vec = res.get();
+                        auto it = modified_vec.begin();
+                        BOOST_CHECK_MESSAGE(*it == 8,"data[0] is wrong: "+ std::to_string(*it));
+                        std::advance(it,100);
+                        BOOST_CHECK_MESSAGE(*it == 8,"data[100] is wrong: "+ std::to_string(*it));
+                        std::advance(it,900);
+                        BOOST_CHECK_MESSAGE(*it == 8,"data[1000] is wrong: "+ std::to_string(*it));
+                        std::advance(it,8999);
+                        BOOST_CHECK_MESSAGE(*it == 8,"data[9999] is wrong: "+ std::to_string(*it));
+                        auto r = std::accumulate(modified_vec.begin(),modified_vec.end(),0,[](int a, int b){return a+b;});
+                        BOOST_CHECK_MESSAGE((r == 80000),
+                                            ("result of parallel_for after if/else was " + std::to_string(r) +
+                                             ", should have been 80000"));
+
+                        // reset
+                        m_data = std::vector<int>(10000,1);
+                        aPromise->set_value();
+           }// callback functor.
+        );
+        return fu;
+    }
+private:
+    std::vector<int> m_data;
+};
+
+class ServantProxy : public boost::asynchronous::servant_proxy<ServantProxy,Servant>
+{
+public:
+    template <class Scheduler>
+    ServantProxy(Scheduler s):
+        boost::asynchronous::servant_proxy<ServantProxy,Servant>(s)
+    {}
+    BOOST_ASYNC_FUTURE_MEMBER(async_if_true)
+    BOOST_ASYNC_FUTURE_MEMBER(async_if_false)
+};
+
+}
+
+
+BOOST_AUTO_TEST_CASE( test_if_true )
+{
+    servant_dtor=false;
+    {
+        auto scheduler = boost::asynchronous::create_shared_scheduler_proxy(new boost::asynchronous::single_thread_scheduler<
+                                                                            boost::asynchronous::lockfree_queue<> >);
+
+        main_thread_id = boost::this_thread::get_id();
+        ServantProxy proxy(scheduler);
+        boost::shared_future<boost::shared_future<void> > fuv = proxy.async_if_true();
+        try
+        {
+            boost::shared_future<void> resfuv = fuv.get();
+            resfuv.get();
+        }
+        catch(...)
+        {
+            BOOST_FAIL( "unexpected exception" );
+        }
+    }
+    BOOST_CHECK_MESSAGE(servant_dtor,"servant dtor not called.");
+}
+
+
+BOOST_AUTO_TEST_CASE( test_if_false )
+{
+    servant_dtor=false;
+    {
+        auto scheduler = boost::asynchronous::create_shared_scheduler_proxy(new boost::asynchronous::single_thread_scheduler<
+                                                                            boost::asynchronous::lockfree_queue<> >);
+
+        main_thread_id = boost::this_thread::get_id();
+        ServantProxy proxy(scheduler);
+        boost::shared_future<boost::shared_future<void> > fuv = proxy.async_if_false();
+        try
+        {
+            boost::shared_future<void> resfuv = fuv.get();
+            resfuv.get();
+        }
+        catch(...)
+        {
+            BOOST_FAIL( "unexpected exception" );
+        }
+    }
+    BOOST_CHECK_MESSAGE(servant_dtor,"servant dtor not called.");
+}
+
