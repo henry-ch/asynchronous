6dde5fd64e274d973d380491a4a9c571ecdb9b56
henry-ch <christophe.j.henry@googlemail.com>
2015-03-19 23:27:23 +0100

    added parallel_geometry_intersection_of_x

---
+++ boost/asynchronous/algorithm/geometry/parallel_geometry_intersection_of_x.hpp
@@ -0,0 +1,344 @@
+// Boost.Asynchronous library
+//  Copyright (C) Christophe Henry 2014
+//
+//  Use, modification and distribution is subject to the Boost
+//  Software License, Version 1.0.  (See accompanying file
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// For more information, see http://www.boost.org
+
+#ifndef BOOST_ASYNCHRONOUS_PARALLEL_GEOMETRY_INTERSECTION_HPP
+#define BOOST_ASYNCHRONOUS_PARALLEL_GEOMETRY_INTERSECTION_HPP
+
+#include <algorithm>
+
+#include <boost/utility/enable_if.hpp>
+#include <boost/geometry.hpp>
+
+#include <boost/asynchronous/callable_any.hpp>
+#include <boost/asynchronous/continuation_task.hpp>
+#include <boost/asynchronous/post.hpp>
+#include <boost/asynchronous/algorithm/detail/safe_advance.hpp>
+#include <boost/asynchronous/detail/metafunctions.hpp>
+#include <boost/asynchronous/algorithm/geometry/parallel_union.hpp>
+
+namespace boost { namespace asynchronous { namespace geometry
+{
+namespace detail
+{
+
+template <class Iterator,class Job>
+struct parallel_geometry_intersection_of_x_helper:
+        public boost::asynchronous::continuation_task<typename std::iterator_traits<Iterator>::value_type>
+{
+    parallel_geometry_intersection_of_x_helper(Iterator beg, Iterator end,
+                            long cutoff,long overlay_cutoff,long partition_cutoff, const std::string& task_name, std::size_t prio)
+        : boost::asynchronous::continuation_task<typename std::iterator_traits<Iterator>::value_type>(task_name),
+          beg_(beg),end_(end),cutoff_(cutoff),overlay_cutoff_(overlay_cutoff),partition_cutoff_(partition_cutoff),prio_(prio)
+    {}    
+
+    void operator()()
+    {
+        boost::asynchronous::continuation_result<typename std::iterator_traits<Iterator>::value_type> task_res = this->this_task_result();
+        // advance up to cutoff
+        Iterator it = boost::asynchronous::detail::find_cutoff(beg_,cutoff_,end_);
+        if (it == end_)
+        {
+            typename std::iterator_traits<Iterator>::value_type final_output;
+            bool first = true;
+            for(auto it = beg_; it != end_; ++it)
+            {
+                if (first)
+                {
+                    final_output = *it;
+                    first = false;
+                }
+                else
+                {
+                    typename std::iterator_traits<Iterator>::value_type result;
+                    boost::geometry::intersection(final_output, *it, result);
+                    final_output = std::move(result);
+                }
+            }
+            task_res.set_value(std::move(final_output));
+        }
+        else
+        {
+            //auto task_name = this->get_name();
+            //auto prio = prio_;
+            //auto cutoff = cutoff_;
+            //auto overlay_cutoff = overlay_cutoff_;
+            //auto partition_cutoff = partition_cutoff_;
+
+            boost::asynchronous::create_callback_continuation_job<Job>(
+                        // called when subtasks are done, set our result
+                        [task_res]
+                        (std::tuple<boost::asynchronous::expected<typename std::iterator_traits<Iterator>::value_type>,
+                                    boost::asynchronous::expected<typename std::iterator_traits<Iterator>::value_type> > res)
+                        {
+                            try
+                            {
+                                auto res0 = std::get<0>(res).get();
+                                auto res1 = std::get<1>(res).get();
+                                typename std::iterator_traits<Iterator>::value_type result;
+                                boost::geometry::intersection(res0, res1, result);
+                                task_res.set_value(std::move(result));
+
+                            }
+                            catch(std::exception& e)
+                            {
+                                task_res.set_exception(boost::copy_exception(e));
+                            }
+                        },
+                        // recursive tasks
+                        parallel_geometry_intersection_of_x_helper<Iterator,Job>
+                            (beg_,it,cutoff_,overlay_cutoff_,partition_cutoff_,this->get_name(),prio_),
+                        parallel_geometry_intersection_of_x_helper<Iterator,Job>
+                            (it,end_,cutoff_,overlay_cutoff_,partition_cutoff_,this->get_name(),prio_)
+            );
+        }
+    }
+    Iterator beg_;
+    Iterator end_;
+    long cutoff_;
+    long overlay_cutoff_;
+    long partition_cutoff_;
+    std::size_t prio_;
+};
+}
+
+template <class Iterator, class Job=BOOST_ASYNCHRONOUS_DEFAULT_JOB>
+boost::asynchronous::detail::callback_continuation<typename std::iterator_traits<Iterator>::value_type,Job>
+parallel_geometry_intersection_of_x(Iterator beg, Iterator end,
+#ifdef BOOST_ASYNCHRONOUS_REQUIRE_ALL_ARGUMENTS
+                    const std::string& task_name, std::size_t prio,
+#else
+                    const std::string& task_name="", std::size_t prio=0,
+#endif
+                    long cutoff=300, long overlay_cutoff=1500, long partition_cutoff=80000)
+{
+    return boost::asynchronous::top_level_callback_continuation_job<typename std::iterator_traits<Iterator>::value_type,Job>
+            (boost::asynchronous::geometry::detail::parallel_geometry_intersection_of_x_helper<Iterator,Job>
+                (beg,end,cutoff,overlay_cutoff,partition_cutoff,task_name,prio));
+
+}
+
+namespace detail
+{
+template <class Range,class Return,class Job>
+struct parallel_geometry_intersection_of_x_range_helper: public boost::asynchronous::continuation_task<Return>
+{
+    template <class Iterator>
+    parallel_geometry_intersection_of_x_range_helper(boost::shared_ptr<Range> range,Iterator beg, Iterator end,
+                            long cutoff,long overlay_cutoff,long partition_cutoff, const std::string& task_name, std::size_t prio)
+        : boost::asynchronous::continuation_task<Return>(task_name),
+          range_(range),begin_(beg),end_(end),cutoff_(cutoff),overlay_cutoff_(overlay_cutoff),partition_cutoff_(partition_cutoff),prio_(prio)
+    {}
+
+    void operator()()
+    {
+        boost::asynchronous::continuation_result<Return> task_res = this->this_task_result();
+        using Iterator = decltype(boost::begin(*range_));
+        // advance up to cutoff
+        auto it = boost::asynchronous::detail::find_cutoff(begin_,cutoff_,end_);
+        if (it == end_)
+        {
+            Return final_output;
+            bool first = true;
+            for(auto it = begin_; it != end_; ++it)
+            {
+                if (first)
+                {
+                    final_output = *it;
+                    first = false;
+                }
+                else
+                {
+                    typename std::iterator_traits<Iterator>::value_type result;
+                    boost::geometry::intersection(final_output, *it, result);
+                    final_output = std::move(result);
+                }
+            }
+            task_res.set_value(std::move(final_output));
+        }
+        else
+        {
+            //auto task_name = this->get_name();
+            //auto prio = prio_;
+            //auto cutoff = cutoff_;
+            //auto overlay_cutoff = overlay_cutoff_;
+            //auto partition_cutoff = partition_cutoff_;
+
+            boost::asynchronous::create_callback_continuation_job<Job>(
+                        // called when subtasks are done, set our result
+                        [task_res/*,task_name,prio,cutoff,overlay_cutoff,partition_cutoff*/]
+                        (std::tuple<boost::asynchronous::expected<Return>,boost::asynchronous::expected<Return> > res)
+                        {
+                            try
+                            {
+                                auto res0 = std::get<0>(res).get();
+                                auto res1 = std::get<1>(res).get();
+                                Return result;
+                                boost::geometry::intersection(res0, res1, result);
+                                task_res.set_value(std::move(result));
+
+                            }
+                            catch(std::exception& e)
+                            {
+                                task_res.set_exception(boost::copy_exception(e));
+                            }
+                        },
+                        // recursive tasks
+                        parallel_geometry_intersection_of_x_range_helper<Range,Return,Job>
+                            (range_,begin_,it,cutoff_,overlay_cutoff_,partition_cutoff_,this->get_name(),prio_),
+                        parallel_geometry_intersection_of_x_range_helper<Range,Return,Job>
+                            (range_,it,end_,cutoff_,overlay_cutoff_,partition_cutoff_,this->get_name(),prio_)
+            );
+        }
+    }
+
+    boost::shared_ptr<Range> range_;
+    decltype(boost::begin(*range_)) begin_;
+    decltype(boost::end(*range_)) end_;
+    long cutoff_;
+    long overlay_cutoff_;
+    long partition_cutoff_;
+    std::size_t prio_;
+};
+}
+
+template <class Range, class Job=BOOST_ASYNCHRONOUS_DEFAULT_JOB>
+auto
+parallel_geometry_intersection_of_x2(Range&& range,
+#ifdef BOOST_ASYNCHRONOUS_REQUIRE_ALL_ARGUMENTS
+                    const std::string& task_name, std::size_t prio,
+#else
+                    const std::string& task_name="", std::size_t prio=0,
+#endif
+                    long cutoff=300, long overlay_cutoff=1500, long partition_cutoff=80000)
+-> typename boost::disable_if<has_is_continuation_task<Range>,
+                              boost::asynchronous::detail::callback_continuation<
+                                typename std::remove_reference<decltype(*boost::begin(range))
+                              >::type,Job>>::type
+{
+    auto r = boost::make_shared<Range>(std::forward<Range>(range));
+    auto beg = boost::begin(*r);
+    auto end = boost::end(*r);
+
+    using Return = typename std::remove_reference<decltype(*boost::begin(range))>::type;
+
+    return boost::asynchronous::top_level_callback_continuation_job<Return,Job>
+            (boost::asynchronous::geometry::detail::parallel_geometry_intersection_of_x_range_helper
+               <Range,Return,Job>
+                (std::move(r),beg,end,cutoff,overlay_cutoff,partition_cutoff,task_name,prio));
+
+}
+
+// version for ranges given as continuation => will return the range as continuation
+namespace detail
+{
+template <class Continuation, class Job,class Enable=void>
+struct parallel_geometry_intersection_of_x_continuation_range_helper: public boost::asynchronous::continuation_task<typename Continuation::return_type>
+{
+    parallel_geometry_intersection_of_x_continuation_range_helper(Continuation c,long cutoff,long overlay_cutoff, long partition_cutoff,
+                        const std::string& task_name, std::size_t prio)
+        :boost::asynchronous::continuation_task<typename Continuation::return_type>(task_name)
+        ,cont_(std::move(c)),cutoff_(cutoff),overlay_cutoff_(overlay_cutoff),partition_cutoff_(partition_cutoff),prio_(prio)
+    {}
+    void operator()()
+    {
+        boost::asynchronous::continuation_result<typename Continuation::return_type> task_res = this->this_task_result();
+        auto cutoff = cutoff_;
+        auto overlay_cutoff = overlay_cutoff_;
+        auto partition_cutoff = partition_cutoff_;
+        auto task_name = this->get_name();
+        auto prio = prio_;
+        cont_.on_done([task_res,cutoff,overlay_cutoff,partition_cutoff,task_name,prio]
+                      (std::tuple<boost::future<typename Continuation::return_type> >&& continuation_res)
+        {
+            try
+            {
+                auto new_continuation = boost::asynchronous::geometry::parallel_geometry_intersection_of_x<typename Continuation::return_type,Job>
+                        (std::move(std::get<0>(continuation_res).get()),task_name,prio,cutoff,overlay_cutoff,partition_cutoff);
+                new_continuation.on_done([task_res](std::tuple<boost::asynchronous::expected<typename Continuation::return_type> >&& new_continuation_res)
+                {
+                    task_res.set_value(std::move(std::get<0>(new_continuation_res).get()));
+                });
+            }
+            catch(std::exception& e)
+            {
+                task_res.set_exception(boost::copy_exception(e));
+            }
+        }
+        );
+        boost::asynchronous::any_continuation ac(std::move(cont_));
+        boost::asynchronous::get_continuations().emplace_front(std::move(ac));
+    }
+    Continuation cont_;
+    long cutoff_;
+    long overlay_cutoff_;
+    long partition_cutoff_;
+    std::size_t prio_;
+};
+// Continuation is a callback continuation
+template <class Continuation, class Job>
+struct parallel_geometry_intersection_of_x_continuation_range_helper<Continuation,Job,typename ::boost::enable_if< has_is_callback_continuation_task<Continuation> >::type>
+        : public boost::asynchronous::continuation_task<typename Continuation::return_type>
+{
+    parallel_geometry_intersection_of_x_continuation_range_helper(Continuation c,long cutoff,long overlay_cutoff, long partition_cutoff,
+                        const std::string& task_name, std::size_t prio)
+        :boost::asynchronous::continuation_task<typename Continuation::return_type>(task_name)
+        ,cont_(std::move(c)),cutoff_(cutoff),overlay_cutoff_(overlay_cutoff),partition_cutoff_(partition_cutoff),prio_(prio)
+    {}
+    void operator()()
+    {
+        boost::asynchronous::continuation_result<typename Continuation::return_type> task_res = this->this_task_result();
+        auto cutoff = cutoff_;
+        auto overlay_cutoff = overlay_cutoff_;
+        auto partition_cutoff = partition_cutoff_;
+        auto task_name = this->get_name();
+        auto prio = prio_;
+        cont_.on_done([task_res,cutoff,overlay_cutoff,partition_cutoff,task_name,prio]
+                      (std::tuple<boost::asynchronous::expected<typename Continuation::return_type> >&& continuation_res)
+        {
+            try
+            {
+                auto new_continuation = boost::asynchronous::geometry::parallel_geometry_intersection_of_x<typename Continuation::return_type,Job>
+                        (std::move(std::get<0>(continuation_res).get()),task_name,prio,cutoff,overlay_cutoff,partition_cutoff);
+                new_continuation.on_done([task_res](std::tuple<boost::asynchronous::expected<typename Continuation::return_type> >&& new_continuation_res)
+                {
+                    task_res.set_value(std::move(std::get<0>(new_continuation_res).get()));
+                });
+            }
+            catch(std::exception& e)
+            {
+                task_res.set_exception(boost::copy_exception(e));
+            }
+        }
+        );
+    }
+    Continuation cont_;
+    long cutoff_;
+    long overlay_cutoff_;
+    long partition_cutoff_;
+    std::size_t prio_;
+};
+}
+// version for ranges given as continuation => will return the range as continuation
+template <class Range, class Job=BOOST_ASYNCHRONOUS_DEFAULT_JOB>
+typename boost::enable_if<has_is_continuation_task<Range>,boost::asynchronous::detail::callback_continuation<typename Range::return_type,Job> >::type
+parallel_geometry_intersection_of_x(Range range,
+#ifdef BOOST_ASYNCHRONOUS_REQUIRE_ALL_ARGUMENTS
+                     const std::string& task_name, std::size_t prio,
+#else
+                     const std::string& task_name="", std::size_t prio=0,
+                     long cutoff=300, long overlay_cutoff=1500, long partition_cutoff=80000)
+#endif
+{
+    return boost::asynchronous::top_level_callback_continuation_job<typename Range::return_type,Job>
+            (boost::asynchronous::geometry::detail::parallel_geometry_intersection_of_x_continuation_range_helper<Range,Job>(
+                 std::move(range),cutoff,overlay_cutoff,partition_cutoff,task_name,prio));
+}
+
+}}}
+#endif // BOOST_ASYNCHRONOUS_PARALLEL_GEOMETRY_INTERSECTION_HPP
---
+++ libs/asynchronous/test/perf/geometry/many_intersections.cpp
@@ -0,0 +1,262 @@
+// Boost.Geometry (aka GGL, Generic Geometry Library)
+// Other Test
+
+// Copyright (c) 2015 Barend Gehrels, Amsterdam, the Netherlands.
+
+// Use, modification and distribution is subject to the Boost Software License,
+// Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+#include <boost/timer.hpp>
+
+#include <boost/geometry.hpp>
+#include <boost/geometry/multi/geometries/multi_geometries.hpp>
+
+#include <boost/random/linear_congruential.hpp>
+#include <boost/random/uniform_int.hpp>
+#include <boost/random/uniform_real.hpp>
+#include <boost/random/variate_generator.hpp>
+
+#include <boost/foreach.hpp>
+#include <boost/asynchronous/post.hpp>
+#include <boost/asynchronous/algorithm/geometry/parallel_geometry_intersection_of_x.hpp>
+#include <boost/asynchronous/queue/lockfree_queue.hpp>
+#include <boost/asynchronous/scheduler_shared_proxy.hpp>
+#include <boost/asynchronous/scheduler/multiqueue_threadpool_scheduler.hpp>
+#include "sinus_buffer2.hpp"
+
+#ifdef TEST_WITH_SVG
+#include <fstream>
+#include <boost/geometry/io/svg/svg_mapper.hpp>
+#endif
+
+
+
+namespace bg = boost::geometry;
+
+
+#ifdef TEST_WITH_SVG
+template <typename Collection, typename Geometry>
+void create_svg(std::string const& filename, Collection const& collection, Geometry const& result)
+{
+    typedef typename boost::range_value<Collection>::type Element;
+    typedef typename bg::point_type<Element>::type point_type;
+    std::ofstream svg(filename.c_str());
+
+    bg::svg_mapper<point_type> mapper(svg, 800, 800);
+
+    mapper.add(result);
+    BOOST_FOREACH(Element const& element, collection)
+    {
+        mapper.add(element);
+    }
+
+    typedef typename boost::range_value<Collection>::type Element;
+    BOOST_FOREACH(Element const& element, collection)
+    {
+        mapper.map(element, "fill-opacity:0.5;fill:rgb(51,51,153);stroke:rgb(0,0,0);stroke-width:1");
+    }
+
+    // Comment if you want to see only input
+    mapper.map(result, "fill:none;stroke:rgb(255,128,0);stroke-width:3");
+}
+#endif
+
+
+// Function to let buffer-distance depend on alpha, e.g.:
+struct intersection_distance_modifier
+{
+    static inline double apply(double distance, double alpha)
+    {
+        return distance * 1.0 + 0.2 * distance * sin(alpha * 6.0);
+    }
+};
+
+template <typename Collection>
+void pairwise_intersections(Collection const& input_collection, Collection& output_collection)
+{
+    output_collection.clear();
+
+    typedef typename boost::range_value<Collection>::type Element;
+
+    std::size_t n = boost::size(input_collection);
+    for (std::size_t i = 1; i < n; i += 2)
+    {
+        Element result;
+        bg::intersection(input_collection[i - 1], input_collection[i], result);
+        output_collection.push_back(result);
+    }
+    if (n % 2 == 1)
+    {
+        output_collection.push_back(input_collection.back());
+    }
+}
+
+void test_many_intersections(int count_x, int count_y, double distance, bool method1, bool method2,
+                             int tpsize,long intersection_of_x_cutoff)
+{
+    typedef bg::model::point<double, 2, bg::cs::cartesian> point;
+    typedef bg::model::polygon<point> polygon_type;
+    typedef bg::model::multi_polygon<polygon_type> multi_polygon_type;
+
+    // Declare main object: a vector of (multi) polygons which will be intersected
+    std::vector<multi_polygon_type> many_polygons;
+
+    // Predefined strategies
+    bg::strategy::buffer::distance_symmetric<double> distance_strategy(distance);
+    bg::strategy::buffer::end_flat end_strategy; // not effectively used
+
+    // Own strategies
+    buffer_join_strategy_sample<intersection_distance_modifier> join_strategy;
+    buffer_point_strategy_sample<intersection_distance_modifier> point_strategy;
+    buffer_side_sample<intersection_distance_modifier> side_strategy;
+
+    // Use a bit of random disturbance in the to be generated grid
+    typedef boost::minstd_rand base_generator_type;
+    base_generator_type generator(12345);
+    boost::uniform_real<> random_range(0.0, 0.5);
+    boost::variate_generator
+    <
+        base_generator_type&,
+        boost::uniform_real<>
+    > random(generator, random_range);
+
+    for (int i = 0; i < count_x; i++)
+    {
+        for (int j = 0; j < count_y; j++)
+        {
+            double x = i * 1.0 + random();
+            double y = j * 1.0 + random();
+
+            point p(x, y);
+
+            multi_polygon_type buffered;
+            // Create the buffer of a point
+            bg::buffer(p, buffered,
+                        distance_strategy, side_strategy,
+                        join_strategy, end_strategy, point_strategy);
+
+            many_polygons.push_back(buffered);
+        }
+    }
+
+
+
+    // Method 1: add geometries one-by-one to the final output (SLOW)
+    double elapsed=0.0;
+    if (method1)
+    {
+        auto start = boost::chrono::high_resolution_clock::now();
+
+        // Sequentially intersection all input geometries
+        multi_polygon_type final_output;
+
+        // Other than union, take care the first one is just assigned
+        bool first = true;
+
+        BOOST_FOREACH(multi_polygon_type const& mp, many_polygons)
+        {
+            if (first)
+            {
+                final_output = mp;
+                first = false;
+            }
+            else
+            {
+                multi_polygon_type result;
+                bg::intersection(final_output, mp, result);
+                final_output = result;
+            }
+        }
+        elapsed = (double)(boost::chrono::nanoseconds(boost::chrono::high_resolution_clock::now() - start).count() / 1000000000.0);
+        std::cout << "Method 1: " << elapsed << " " << bg::area(final_output) << std::endl;
+
+        #ifdef TEST_WITH_SVG
+        create_svg("/tmp/many_intersections1.svg", many_polygons, final_output);
+        #endif
+    }
+
+    // Method 2: add geometries pair-wise (much faster)
+    double elapsed2=0.0;
+    if (method2)
+    {
+        auto start = boost::chrono::high_resolution_clock::now();
+
+        std::vector<multi_polygon_type> final_output;
+        pairwise_intersections(many_polygons, final_output);
+
+        while(final_output.size() > 1)
+        {
+            std::vector<multi_polygon_type> input = final_output;
+            pairwise_intersections(input, final_output);
+        }
+
+        BOOST_ASSERT(final_output.size() == 1);
+        elapsed2 = (double)(boost::chrono::nanoseconds(boost::chrono::high_resolution_clock::now() - start).count() / 1000000000.0);
+        std::cout << "Method 2: " << elapsed2 << " " << bg::area(final_output.front()) << std::endl;
+
+
+        #ifdef TEST_WITH_SVG
+        create_svg("/tmp/many_intersections2.svg", many_polygons, final_output.front());
+        #endif
+    }
+
+    // Method 3: in parallel
+    double elapsed3=0.0;
+    auto pool = boost::asynchronous::create_shared_scheduler_proxy(
+                new boost::asynchronous::multiqueue_threadpool_scheduler<
+                        boost::asynchronous::lockfree_queue<>/*,
+                        boost::asynchronous::default_find_position< boost::asynchronous::sequential_push_policy>,
+                        boost::asynchronous::no_cpu_load_saving*/
+                    >(tpsize,64));
+
+    {
+        auto start = boost::chrono::high_resolution_clock::now();
+
+        /*auto beg = many_polygons.begin();
+        auto end = many_polygons.end();
+
+        boost::future<multi_polygon_type> fu = boost::asynchronous::post_future(pool,
+        [beg,end,intersection_of_x_cutoff]()
+        {
+            return boost::asynchronous::geometry::parallel_geometry_intersection_of_x<decltype(beg),BOOST_ASYNCHRONOUS_DEFAULT_JOB>
+                    (beg,end,"",0,intersection_of_x_cutoff);
+        }
+        ,"",0);*/
+
+        boost::future<multi_polygon_type> fu = boost::asynchronous::post_future(pool,
+        [intersection_of_x_cutoff,many_polygons=std::move(many_polygons)/*,overlay_cutoff,partition_cutoff*/]()mutable
+        {
+            return boost::asynchronous::geometry::parallel_geometry_intersection_of_x2<std::vector<multi_polygon_type>,BOOST_ASYNCHRONOUS_DEFAULT_JOB>
+                    (std::move(many_polygons),"",0,intersection_of_x_cutoff/*,overlay_cutoff,partition_cutoff*/);
+        }
+        ,"",0);
+        multi_polygon_type final_output = std::move(fu.get());
+        elapsed3 = (double)(boost::chrono::nanoseconds(boost::chrono::high_resolution_clock::now() - start).count() / 1000000000.0);
+
+        std::cout << "Method 3: " << elapsed3 << " " << bg::area(final_output) << std::endl;
+        #ifdef TEST_WITH_SVG
+        create_svg("/tmp/many_intersections3.svg", many_polygons, final_output);
+        #endif
+    }
+
+    std::cout << "speedup Method3/Method1: " << elapsed / elapsed3 << std::endl;
+}
+
+int main(int argc, char** argv)
+{
+    int count_x = argc > 1 ? atol(argv[1]) : 50;
+    int count_y = argc > 2 ? atol(argv[2]) : count_x;
+    double distance = argc > 3 ? atof(argv[3]) : double(count_x);
+    // for asynchronous
+    int tpsize = argc > 4 ? atol(argv[4]) : 8;
+    long intersection_of_x_cutoff = argc > 5 ? atol(argv[5]) : 300;
+
+    std::cout << "tpsize=" << tpsize << std::endl;
+    std::cout << "intersection_of_x_cutoff=" << intersection_of_x_cutoff << std::endl;
+
+    std::cout << "Testing for " << count_x << " x " << count_y <<  " with " << distance << std::endl;
+    test_many_intersections(count_x, count_y, distance, true, false,tpsize,intersection_of_x_cutoff);
+
+    return 0;
+}
