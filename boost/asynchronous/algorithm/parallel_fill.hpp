3581cdd27f60cc069235112df61f0032a2a0ee03
henry-ch <christophe.j.henry@googlemail.com>
2015-04-26 22:21:43 +0200

    added parallel_fill and parallel_generate

---
+++ boost/asynchronous/algorithm/parallel_fill.hpp
@@ -0,0 +1,100 @@
+// Boost.Asynchronous library
+//  Copyright (C) Christophe Henry, Tobias Holl 2015
+//
+//  Use, modification and distribution is subject to the Boost
+//  Software License, Version 1.0.  (See accompanying file
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// For more information, see http://www.boost.org
+
+#ifndef BOOST_ASYNCHRONOUS_PARALLEL_FILL_HPP
+#define BOOST_ASYNCHRONOUS_PARALLEL_FILL_HPP
+
+#include <boost/utility/enable_if.hpp>
+#include <boost/asynchronous/algorithm/parallel_for.hpp>
+#include <boost/asynchronous/callable_any.hpp>
+#include <boost/asynchronous/continuation_task.hpp>
+#include <boost/asynchronous/detail/any_interruptible.hpp>
+#include <boost/asynchronous/detail/continuation_impl.hpp>
+#include <boost/asynchronous/post.hpp>
+#include <boost/asynchronous/scheduler/serializable_task.hpp>
+#include <boost/asynchronous/detail/metafunctions.hpp>
+
+
+namespace boost { namespace asynchronous {
+
+// Iterators
+template <class Iterator, class Value, class Job=BOOST_ASYNCHRONOUS_DEFAULT_JOB>
+boost::asynchronous::detail::callback_continuation<void, Job>
+parallel_fill(Iterator beg, Iterator end, const Value& value, long cutoff,
+#ifdef BOOST_ASYNCHRONOUS_REQUIRE_ALL_ARGUMENTS
+            const std::string& task_name, std::size_t prio)
+#else
+            const std::string& task_name="", std::size_t prio=0)
+#endif
+{
+    auto l = [value](const typename std::iterator_traits<Iterator>::value_type& ref)
+    {
+        const_cast<typename std::iterator_traits<Iterator>::value_type&>(ref) = value;
+    };
+    return boost::asynchronous::parallel_for<Iterator,decltype(l),Job>(beg, end,std::move(l),
+                                                                       cutoff, task_name, prio);
+}
+
+// Moved range
+template <class Range, class Value, class Job=BOOST_ASYNCHRONOUS_DEFAULT_JOB>
+typename boost::disable_if<has_is_continuation_task<Range>, boost::asynchronous::detail::callback_continuation<Range, Job>>::type
+parallel_fill(Range&& range, const Value& value, long cutoff,
+#ifdef BOOST_ASYNCHRONOUS_REQUIRE_ALL_ARGUMENTS
+             const std::string& task_name, std::size_t prio)
+#else
+             const std::string& task_name="", std::size_t prio=0)
+#endif
+{
+    auto l = [value](const typename std::iterator_traits<decltype(boost::begin(range))>::value_type& ref)
+    {
+        const_cast<typename std::iterator_traits<decltype(boost::begin(range))>::value_type&>(ref) = value;
+    };
+    return boost::asynchronous::parallel_for<Range,decltype(l),Job>(std::forward<Range>(range),std::move(l),
+                                                                    cutoff, task_name, prio);
+}
+
+// Range reference
+template <class Range, class Value, class Job=BOOST_ASYNCHRONOUS_DEFAULT_JOB>
+typename boost::disable_if<has_is_continuation_task<Range>, boost::asynchronous::detail::callback_continuation<void, Job>>::type
+parallel_fill(const Range& range, const Value& value, long cutoff,
+#ifdef BOOST_ASYNCHRONOUS_REQUIRE_ALL_ARGUMENTS
+             const std::string& task_name, std::size_t prio)
+#else
+             const std::string& task_name="", std::size_t prio=0)
+#endif
+{
+    auto l = [value](const typename std::iterator_traits<decltype(boost::begin(range))>::value_type& ref)
+    {
+        const_cast<typename std::iterator_traits<decltype(boost::begin(range))>::value_type&>(ref) = value;
+    };
+    return boost::asynchronous::parallel_for<Range,decltype(l),Job>(range,std::move(l),
+                                                                    cutoff, task_name, prio);
+}
+
+// Continuations
+template <class Range, class Value, class Job=BOOST_ASYNCHRONOUS_DEFAULT_JOB>
+typename boost::enable_if<has_is_continuation_task<Range>, boost::asynchronous::detail::callback_continuation<typename Range::return_type, Job>>::type
+parallel_fill(Range range, const Value& value, long cutoff,
+#ifdef BOOST_ASYNCHRONOUS_REQUIRE_ALL_ARGUMENTS
+             const std::string& task_name, std::size_t prio)
+#else
+             const std::string& task_name="", std::size_t prio=0)
+#endif
+{
+    auto l = [value](typename std::iterator_traits<decltype(boost::begin(std::declval<typename Range::return_type>()))>::value_type const& ref)
+    {
+        const_cast<typename std::iterator_traits<decltype(boost::begin(std::declval<typename Range::return_type>()))>::value_type&>(ref) = value;
+    };
+    return boost::asynchronous::parallel_for<Range,decltype(l),Job>(range,std::move(l),
+                                                                    cutoff, task_name, prio);
+}
+
+}}
+
+#endif // BOOST_ASYNCHRONOUS_PARALLEL_FILL_HPP
---
+++ libs/asynchronous/doc/examples/example_parallel_fill.cpp
@@ -0,0 +1,101 @@
+#include <iostream>
+#include <boost/enable_shared_from_this.hpp>
+
+#include <boost/asynchronous/scheduler/single_thread_scheduler.hpp>
+#include <boost/asynchronous/queue/lockfree_queue.hpp>
+#include <boost/asynchronous/scheduler_shared_proxy.hpp>
+#include <boost/asynchronous/scheduler/threadpool_scheduler.hpp>
+#include <boost/asynchronous/servant_proxy.hpp>
+#include <boost/asynchronous/post.hpp>
+#include <boost/asynchronous/checks.hpp>
+#include <boost/asynchronous/trackable_servant.hpp>
+#include <boost/asynchronous/algorithm/parallel_fill.hpp>
+
+using namespace std;
+
+namespace
+{
+struct Servant : boost::asynchronous::trackable_servant<>
+{
+    // optional, ctor is simple enough not to be posted
+    typedef int simple_ctor;
+    Servant(boost::asynchronous::any_weak_scheduler<> scheduler)
+        : boost::asynchronous::trackable_servant<>(scheduler,
+                                               // threadpool with 3 threads and a simple threadsafe_list queue
+                                               boost::asynchronous::make_shared_scheduler_proxy<
+                                                   boost::asynchronous::threadpool_scheduler<
+                                                           boost::asynchronous::lockfree_queue<>>>(6))
+        // for testing purpose
+        , m_promise(new boost::promise<void>)
+        , m_data(10000,1)
+    {
+    }
+    // called when task done, in our thread
+    void on_callback()
+    {
+        std::cout << "Callback in our (safe) single-thread scheduler" << std::endl;
+        std::cout << "The following values should all be 42." << std::endl;
+        auto it = m_data.begin();
+        std::cout << "m_data[0]= " << *it << std::endl;
+        std::advance(it,100);
+        std::cout << "m_data[100]= " << *it << std::endl;
+        std::advance(it,900);
+        std::cout << "m_data[1000]= " << *it << std::endl;
+        std::advance(it,8999);
+        std::cout << "m_data[9999]= " << *it << std::endl;
+        // inform test caller
+        m_promise->set_value();
+    }
+    // call to this is posted and executes in our (safe) single-thread scheduler
+    boost::shared_future<void> start_async_work()
+    {
+        // for testing purpose
+        boost::shared_future<void> fu = m_promise->get_future();
+        // start long tasks in threadpool (first lambda) and callback in our thread
+        // we know the data will be alive until the end so we can use "this"
+        post_callback(
+               [this](){
+                        return boost::asynchronous::parallel_fill(this->m_data.begin(), this->m_data.end(), 42, 1500);
+                      },// work
+               // the lambda calls Servant, just to show that all is safe, Servant is alive if this is called
+               [this](boost::asynchronous::expected<void> /*res*/){
+                            this->on_callback();
+               }// callback functor.
+        );
+        return fu;
+    }
+private:
+// for testing
+boost::shared_ptr<boost::promise<void> > m_promise;
+std::vector<int> m_data;
+};
+class ServantProxy : public boost::asynchronous::servant_proxy<ServantProxy,Servant>
+{
+public:
+    template <class Scheduler>
+    ServantProxy(Scheduler s):
+        boost::asynchronous::servant_proxy<ServantProxy,Servant>(s)
+    {}
+    // caller will get a future
+    BOOST_ASYNC_FUTURE_MEMBER(start_async_work)
+};
+
+}
+
+void example_fill()
+{
+    std::cout << "example_fill" << std::endl;
+    {
+        // a single-threaded world, where Servant will live.
+        auto scheduler = boost::asynchronous::make_shared_scheduler_proxy<
+                                boost::asynchronous::single_thread_scheduler<
+                                     boost::asynchronous::lockfree_queue<>>>();
+        {
+            ServantProxy proxy(scheduler);
+            boost::shared_future<boost::shared_future<void> > fu = proxy.start_async_work();
+            boost::shared_future<void> resfu = fu.get();
+            resfu.get();
+        }
+    }
+    std::cout << "end example_fill" << std::endl << std::endl;
+}
