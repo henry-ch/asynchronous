4a9b68596f0e2c1f4cf37d133fb6fdf002039b4d
henry-ch <christophe.j.henry@googlemail.com>
2015-05-11 21:38:28 +0200

    added continuation version for parallel_replace_copy

--- boost/asynchronous/algorithm/parallel_replace_copy.hpp
+++ boost/asynchronous/algorithm/parallel_replace_copy.hpp
@@ -42,6 +42,70 @@ parallel_replace_copy_if(Iterator beg,Iterator end,Iterator2 beg2, Func func, T 
             (beg,end,beg2,std::move(l),cutoff,task_name,prio);
 }
 
+// version for ranges returned as continuations
+namespace detail
+{
+template <class Continuation, class Iterator2, class Func, class T,class Job>
+struct parallel_replace_copy_if_continuation_helper: public boost::asynchronous::continuation_task<Iterator2>
+{
+    parallel_replace_copy_if_continuation_helper(Continuation c,Iterator2 beg2,Func func, T const& new_value,long cutoff,
+                                                 const std::string& task_name, std::size_t prio)
+        : boost::asynchronous::continuation_task<Iterator2>(task_name)
+        , cont_(std::move(c)),res_it_(beg2),func_(std::move(func)),new_value_(new_value),cutoff_(cutoff),prio_(prio)
+    {}
+    void operator()()
+    {
+        boost::asynchronous::continuation_result<Iterator2> task_res = this->this_task_result();
+        auto res_it = res_it_;
+        auto func(std::move(func_));
+        auto new_value = new_value_;
+        auto cutoff = cutoff_;
+        auto task_name = this->get_name();
+        auto prio = prio_;
+        cont_.on_done([task_res,res_it,func,new_value,cutoff,task_name,prio]
+                      (std::tuple<boost::asynchronous::expected<typename Continuation::return_type> >&& continuation_res)
+        {
+            try
+            {
+                auto res = boost::make_shared<typename Continuation::return_type>(std::move(std::get<0>(continuation_res).get()));
+                auto new_continuation = boost::asynchronous::parallel_replace_copy_if
+                        <decltype(boost::begin(std::declval<typename Continuation::return_type>())), Iterator2,T,Func, Job>
+                            (boost::begin(*res),boost::end(*res),res_it,func,new_value,cutoff,task_name,prio);
+                new_continuation.on_done([res,task_res](std::tuple<boost::asynchronous::expected<Iterator2> >&& new_continuation_res)
+                {
+                    task_res.set_value(std::move(std::get<0>(new_continuation_res).get()));
+                });
+            }
+            catch(std::exception& e)
+            {
+                task_res.set_exception(boost::copy_exception(e));
+            }
+        });
+    }
+    Continuation cont_;
+    Iterator2 res_it_;
+    Func func_;
+    T new_value_;
+    long cutoff_;
+    std::size_t prio_;
+};
+}
+
+template <class Range, class Iterator2, class Func, class T, class Job=BOOST_ASYNCHRONOUS_DEFAULT_JOB>
+typename boost::enable_if<has_is_continuation_task<Range>,boost::asynchronous::detail::callback_continuation<Iterator2,Job> >::type
+parallel_replace_copy_if(Range range,Iterator2 beg2,Func func, T const& new_value, long cutoff,
+#ifdef BOOST_ASYNCHRONOUS_REQUIRE_ALL_ARGUMENTS
+             const std::string& task_name, std::size_t prio)
+#else
+             const std::string& task_name="", std::size_t prio=0)
+#endif
+{
+    return boost::asynchronous::top_level_callback_continuation_job<Iterator2,Job>
+            (boost::asynchronous::detail::parallel_replace_copy_if_continuation_helper<Range,Iterator2,Func,T,Job>
+               (std::move(range),beg2,std::move(func),new_value,cutoff,task_name,prio));
+}
+
+
 template <class Iterator, class Iterator2, class T, class Job=BOOST_ASYNCHRONOUS_DEFAULT_JOB>
 boost::asynchronous::detail::callback_continuation<Iterator2,Job>
 parallel_replace_copy(Iterator beg,Iterator end,Iterator2 beg2, T const& old_value, T const& new_value,long cutoff,
@@ -65,6 +129,69 @@ parallel_replace_copy(Iterator beg,Iterator end,Iterator2 beg2, T const& old_val
 
     return boost::asynchronous::parallel_transform<Iterator,Iterator2,decltype(l),Job>
             (beg,end,beg2,std::move(l),cutoff,task_name,prio);
+}
+
+// version for ranges returned as continuations
+namespace detail
+{
+template <class Continuation, class Iterator2, class T,class Job>
+struct parallel_replace_copy_continuation_helper: public boost::asynchronous::continuation_task<Iterator2>
+{
+    parallel_replace_copy_continuation_helper(Continuation c,Iterator2 beg2,T const& old_value, T const& new_value,long cutoff,
+                                                 const std::string& task_name, std::size_t prio)
+        : boost::asynchronous::continuation_task<Iterator2>(task_name)
+        , cont_(std::move(c)),res_it_(beg2),old_value_(old_value),new_value_(new_value),cutoff_(cutoff),prio_(prio)
+    {}
+    void operator()()
+    {
+        boost::asynchronous::continuation_result<Iterator2> task_res = this->this_task_result();
+        auto res_it = res_it_;
+        auto old_value = old_value_;
+        auto new_value = new_value_;
+        auto cutoff = cutoff_;
+        auto task_name = this->get_name();
+        auto prio = prio_;
+        cont_.on_done([task_res,res_it,old_value,new_value,cutoff,task_name,prio]
+                      (std::tuple<boost::asynchronous::expected<typename Continuation::return_type> >&& continuation_res)
+        {
+            try
+            {
+                auto res = boost::make_shared<typename Continuation::return_type>(std::move(std::get<0>(continuation_res).get()));
+                auto new_continuation = boost::asynchronous::parallel_replace_copy
+                        <decltype(boost::begin(std::declval<typename Continuation::return_type>())), Iterator2,T, Job>
+                            (boost::begin(*res),boost::end(*res),res_it,old_value,new_value,cutoff,task_name,prio);
+                new_continuation.on_done([res,task_res](std::tuple<boost::asynchronous::expected<Iterator2> >&& new_continuation_res)
+                {
+                    task_res.set_value(std::move(std::get<0>(new_continuation_res).get()));
+                });
+            }
+            catch(std::exception& e)
+            {
+                task_res.set_exception(boost::copy_exception(e));
+            }
+        });
+    }
+    Continuation cont_;
+    Iterator2 res_it_;
+    T old_value_;
+    T new_value_;
+    long cutoff_;
+    std::size_t prio_;
+};
+}
+
+template <class Range, class Iterator2, class T, class Job=BOOST_ASYNCHRONOUS_DEFAULT_JOB>
+typename boost::enable_if<has_is_continuation_task<Range>,boost::asynchronous::detail::callback_continuation<Iterator2,Job> >::type
+parallel_replace_copy(Range range,Iterator2 beg2, T const& old_value, T const& new_value, long cutoff,
+#ifdef BOOST_ASYNCHRONOUS_REQUIRE_ALL_ARGUMENTS
+             const std::string& task_name, std::size_t prio)
+#else
+             const std::string& task_name="", std::size_t prio=0)
+#endif
+{
+    return boost::asynchronous::top_level_callback_continuation_job<Iterator2,Job>
+            (boost::asynchronous::detail::parallel_replace_copy_continuation_helper<Range,Iterator2,T,Job>
+               (std::move(range),beg2,old_value,new_value,cutoff,task_name,prio));
 }
 
 }}
--- libs/asynchronous/test/test_parallel_replace_copy.cpp
+++ libs/asynchronous/test/test_parallel_replace_copy.cpp
@@ -20,6 +20,7 @@
 #include <boost/asynchronous/post.hpp>
 #include <boost/asynchronous/trackable_servant.hpp>
 #include <boost/asynchronous/algorithm/parallel_replace_copy.hpp>
+#include <boost/asynchronous/algorithm/parallel_for.hpp>
 
 #include "test_common.hpp"
 
@@ -139,6 +140,114 @@ struct Servant : boost::asynchronous::trackable_servant<>
         return fu;
     }
 
+    boost::shared_future<void> parallel_replace_copy_if_continuation()
+    {
+        m_data = std::vector<int>(10000,1);
+        std::vector<int> more(5000,2);
+        m_result_data = std::vector<int>(15000,1);
+        m_data.insert(m_data.end(),more.begin(),more.end());
+        auto copy_vec = m_result_data;
+        for (auto& i: copy_vec)
+        {
+            i += 2;
+        }
+
+        BOOST_CHECK_MESSAGE(main_thread_id!=boost::this_thread::get_id(),"servant parallel_replace not posted.");
+        // we need a promise to inform caller when we're done
+        boost::shared_ptr<boost::promise<void> > aPromise(new boost::promise<void>);
+        boost::shared_future<void> fu = aPromise->get_future();
+        boost::asynchronous::any_shared_scheduler_proxy<> tp =get_worker();
+        std::vector<boost::thread::id> ids = tp.thread_ids();
+        // start long tasks
+        post_callback(
+           [ids,this](){
+                    BOOST_CHECK_MESSAGE(main_thread_id!=boost::this_thread::get_id(),"servant work not posted.");
+
+                    BOOST_CHECK_MESSAGE(contains_id(ids.begin(),ids.end(),boost::this_thread::get_id()),"task executed in the wrong thread");
+                    return boost::asynchronous::parallel_replace_copy_if(
+                                boost::asynchronous::parallel_for(std::move(this->m_data),
+                                                                 [](int const& i)
+                                                                 {
+                                                                    const_cast<int&>(i) += 2;
+                                                                 },1500),
+                                this->m_result_data.begin(),
+                                [](int const& i)
+                                {
+                                  return i == 4;
+                                },
+                                3, //new value if i==4
+                                1500);
+                    },// work
+           [aPromise,ids,copy_vec,this](boost::asynchronous::expected<Iterator> res)mutable{
+                        BOOST_CHECK_MESSAGE(!res.has_exception(),"servant work threw an exception.");
+                        BOOST_CHECK_MESSAGE(main_thread_id!=boost::this_thread::get_id(),"servant callback in main thread.");
+                        BOOST_CHECK_MESSAGE(!contains_id(ids.begin(),ids.end(),boost::this_thread::get_id()),"task callback executed in the wrong thread(pool)");
+                        BOOST_CHECK_MESSAGE(!res.has_exception(),"servant work threw an exception.");
+
+                        std::replace_copy_if(this->m_data.begin(),this->m_data.end(),copy_vec.begin(),
+                                             [](int const& i)
+                                             {
+                                               return i == 4;
+                                             },3);
+
+                        BOOST_CHECK_MESSAGE((copy_vec == this->m_result_data),"parallel_replace_copy_if is wrong");
+                        aPromise->set_value();
+           }// callback functor.
+        );
+        return fu;
+    }
+
+    boost::shared_future<void> parallel_replace_copy_continuation()
+    {
+        m_data = std::vector<int>(10000,1);
+        std::vector<int> more(5000,2);
+        m_result_data = std::vector<int>(15000,1);
+        m_data.insert(m_data.end(),more.begin(),more.end());
+        auto copy_vec = m_result_data;
+        for (auto& i: copy_vec)
+        {
+            i += 2;
+        }
+
+        BOOST_CHECK_MESSAGE(main_thread_id!=boost::this_thread::get_id(),"servant parallel_replace not posted.");
+        // we need a promise to inform caller when we're done
+        boost::shared_ptr<boost::promise<void> > aPromise(new boost::promise<void>);
+        boost::shared_future<void> fu = aPromise->get_future();
+        boost::asynchronous::any_shared_scheduler_proxy<> tp =get_worker();
+        std::vector<boost::thread::id> ids = tp.thread_ids();
+        // start long tasks
+        post_callback(
+           [ids,this](){
+                    BOOST_CHECK_MESSAGE(main_thread_id!=boost::this_thread::get_id(),"servant work not posted.");
+
+                    BOOST_CHECK_MESSAGE(contains_id(ids.begin(),ids.end(),boost::this_thread::get_id()),"task executed in the wrong thread");
+                    return boost::asynchronous::parallel_replace_copy(
+                                boost::asynchronous::parallel_for(std::move(this->m_data),
+                                                                 [](int const& i)
+                                                                 {
+                                                                    const_cast<int&>(i) += 2;
+                                                                 },1500),
+                                this->m_result_data.begin(),
+                                4,
+                                3, //new value if i==4
+                                1500);
+                    },// work
+           [aPromise,ids,copy_vec,this](boost::asynchronous::expected<Iterator> res)mutable{
+                        BOOST_CHECK_MESSAGE(!res.has_exception(),"servant work threw an exception.");
+                        BOOST_CHECK_MESSAGE(main_thread_id!=boost::this_thread::get_id(),"servant callback in main thread.");
+                        BOOST_CHECK_MESSAGE(!contains_id(ids.begin(),ids.end(),boost::this_thread::get_id()),"task callback executed in the wrong thread(pool)");
+                        BOOST_CHECK_MESSAGE(!res.has_exception(),"servant work threw an exception.");
+
+                        std::replace_copy(this->m_data.begin(),this->m_data.end(),copy_vec.begin(),
+                                             4,3);
+
+                        BOOST_CHECK_MESSAGE((copy_vec == this->m_result_data),"parallel_replace_copy_if is wrong");
+                        aPromise->set_value();
+           }// callback functor.
+        );
+        return fu;
+    }
+
 private:
     std::vector<int> m_data;
     std::vector<int> m_result_data;
@@ -153,6 +262,8 @@ public:
     {}
     BOOST_ASYNC_FUTURE_MEMBER(parallel_replace_copy_if_iterators)
     BOOST_ASYNC_FUTURE_MEMBER(parallel_replace_copy_iterators)
+    BOOST_ASYNC_FUTURE_MEMBER(parallel_replace_copy_if_continuation)
+    BOOST_ASYNC_FUTURE_MEMBER(parallel_replace_copy_continuation)
 };
 
 }
@@ -200,3 +311,48 @@ BOOST_AUTO_TEST_CASE( test_parallel_replace_copy )
     }
     BOOST_CHECK_MESSAGE(servant_dtor,"servant dtor not called.");
 }
+
+BOOST_AUTO_TEST_CASE( test_parallel_replace_copy_if_continuation )
+{
+    servant_dtor=false;
+    {
+        auto scheduler = boost::asynchronous::make_shared_scheduler_proxy<boost::asynchronous::single_thread_scheduler<
+                                                                            boost::asynchronous::lockfree_queue<>>>();
+        main_thread_id = boost::this_thread::get_id();
+        ServantProxy proxy(scheduler);
+        boost::shared_future<boost::shared_future<void> > fuv = proxy.parallel_replace_copy_if_continuation();
+        try
+        {
+            boost::shared_future<void> resfuv = fuv.get();
+            resfuv.get();
+        }
+        catch(...)
+        {
+            BOOST_FAIL( "unexpected exception" );
+        }
+    }
+    BOOST_CHECK_MESSAGE(servant_dtor,"servant dtor not called.");
+}
+
+BOOST_AUTO_TEST_CASE( test_parallel_replace_copy_continuation )
+{
+    servant_dtor=false;
+    {
+        auto scheduler = boost::asynchronous::make_shared_scheduler_proxy<boost::asynchronous::single_thread_scheduler<
+                                                                            boost::asynchronous::lockfree_queue<>>>();
+        main_thread_id = boost::this_thread::get_id();
+        ServantProxy proxy(scheduler);
+        boost::shared_future<boost::shared_future<void> > fuv = proxy.parallel_replace_copy_continuation();
+        try
+        {
+            boost::shared_future<void> resfuv = fuv.get();
+            resfuv.get();
+        }
+        catch(...)
+        {
+            BOOST_FAIL( "unexpected exception" );
+        }
+    }
+    BOOST_CHECK_MESSAGE(servant_dtor,"servant dtor not called.");
+}
+
