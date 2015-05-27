7402c98135d7f867872fda3a6969dbba3b1a06e0
henry-ch <christophe.j.henry@googlemail.com>
2015-05-27 22:23:31 +0200

    added parallel_search_n

---
+++ boost/asynchronous/algorithm/parallel_search_n.hpp
@@ -0,0 +1,163 @@
+// Boost.Asynchronous library
+//  Copyright (C) Christophe Henry 2015
+//
+//  Use, modification and distribution is subject to the Boost
+//  Software License, Version 1.0.  (See accompanying file
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// For more information, see http://www.boost.org
+#ifndef BOOST_ASYNCHRONOUS_PARALLEL_SEARCH_N_HPP
+#define BOOST_ASYNCHRONOUS_PARALLEL_SEARCH_N_HPP
+
+#include <algorithm>
+#include <vector>
+#include <iterator> // for std::iterator_traits
+
+#include <boost/utility/enable_if.hpp>
+#include <boost/serialization/vector.hpp>
+
+#include <boost/asynchronous/detail/any_interruptible.hpp>
+#include <boost/asynchronous/callable_any.hpp>
+#include <boost/asynchronous/detail/continuation_impl.hpp>
+#include <boost/asynchronous/continuation_task.hpp>
+#include <boost/asynchronous/post.hpp>
+#include <boost/asynchronous/scheduler/serializable_task.hpp>
+#include <boost/asynchronous/algorithm/detail/safe_advance.hpp>
+#include <boost/asynchronous/detail/metafunctions.hpp>
+
+#include <boost/range/begin.hpp>
+#include <boost/range/end.hpp>
+#include <boost/range/iterator_range.hpp>
+#include <boost/range/algorithm_ext/push_back.hpp>
+namespace boost { namespace asynchronous
+{
+
+// version for iterators => will return nothing
+namespace detail
+{
+template <class Iterator1,class Size, class T, class Func, class Job>
+struct parallel_search_n_helper: public boost::asynchronous::continuation_task<Iterator1>
+{
+    parallel_search_n_helper(Iterator1 beg1, Iterator1 end1,Size count, const T& value,Func func,
+                                  long cutoff,const std::string& task_name, std::size_t prio)
+        : boost::asynchronous::continuation_task<Iterator1>(task_name)
+        , beg1_(beg1),end1_(end1),count_(count),value_(value),func_(std::move(func)),cutoff_(cutoff),prio_(prio)
+    {}
+
+    void operator()()const
+    {
+        boost::asynchronous::continuation_result<Iterator1> task_res = this->this_task_result();
+        // advance up to cutoff
+        Iterator1 it = boost::asynchronous::detail::find_cutoff(beg1_,cutoff_,end1_);
+        // if not at end, recurse, otherwise execute here
+        if (it == end1_)
+        {
+            task_res.set_value(std::search_n(beg1_,end1_,count_,value_,func_));
+        }
+        else
+        {
+            auto beg1 = beg1_;
+            auto end1 = end1_;
+            auto count = count_;
+            auto value = value_;
+            auto func = std::move(func_);
+            boost::asynchronous::create_callback_continuation_job<Job>(
+                        // called when subtasks are done, set our result
+                        [task_res,it,beg1,end1,count,value,func]
+                        (std::tuple<boost::asynchronous::expected<Iterator1>,boost::asynchronous::expected<Iterator1>> res)
+                        {
+                            try
+                            {
+                                auto r1 = std::move(std::get<0>(res).get());
+                                auto r2 = std::move(std::get<1>(res).get());
+                                if (r1 != it)
+                                {
+                                    // found in first part
+                                    task_res.set_value(std::move(r1));
+                                }
+                                else
+                                {
+                                    // check in overlap region
+                                    auto itbeg = beg1;
+                                    auto itend = it;
+                                    std::advance(itbeg, std::distance(beg1,it) - count);
+                                    std::advance(itend, count);
+                                    auto itoverlap = std::search_n(itbeg,itend,count,value,func);
+                                    if(itoverlap != itend)
+                                    {
+                                        task_res.set_value(std::move(itoverlap));
+                                    }
+                                    // check in second part
+                                    else if (r2 != end1)
+                                    {
+                                        task_res.set_value(std::move(r2));
+                                    }
+                                    // nowhere found => end
+                                    else
+                                    {
+                                        task_res.set_value(std::move(end1));
+                                    }
+                                }
+                            }
+                            catch(std::exception& e)
+                            {
+                                task_res.set_exception(boost::copy_exception(e));
+                            }
+                        },
+                        // recursive tasks
+                        parallel_search_n_helper<Iterator1,Size,T,Func,Job>
+                                (beg1_,it,count_,value_,func_,cutoff_,this->get_name(),prio_),
+                        parallel_search_n_helper<Iterator1,Size,T,Func,Job>
+                                (it,end1_,count_,value_,func_,cutoff_,this->get_name(),prio_)
+               );
+        }
+    }
+
+    Iterator1 beg1_;
+    Iterator1 end1_;
+    Size count_;
+    T value_;
+    Func func_;
+    long cutoff_;
+    std::size_t prio_;
+};
+}
+
+template <class Iterator1, class Size, class T, class Func, class Job=BOOST_ASYNCHRONOUS_DEFAULT_JOB>
+boost::asynchronous::detail::callback_continuation<Iterator1,Job>
+parallel_search_n(Iterator1 beg1,Iterator1 end1, Size count, const T& value,Func func,long cutoff,
+#ifdef BOOST_ASYNCHRONOUS_REQUIRE_ALL_ARGUMENTS
+             const std::string& task_name, std::size_t prio)
+#else
+             const std::string& task_name="", std::size_t prio=0)
+#endif
+{
+    return boost::asynchronous::top_level_callback_continuation_job<Iterator1,Job>
+            (boost::asynchronous::detail::parallel_search_n_helper<Iterator1,Size,T,Func,Job>
+               (beg1,end1,count,value,std::move(func),cutoff,task_name,prio));
+}
+
+template <class Iterator1, class Size, class T, class Job=BOOST_ASYNCHRONOUS_DEFAULT_JOB>
+boost::asynchronous::detail::callback_continuation<Iterator1,Job>
+parallel_search_n(Iterator1 beg1,Iterator1 end1, Size count, const T& value,long cutoff,
+#ifdef BOOST_ASYNCHRONOUS_REQUIRE_ALL_ARGUMENTS
+             const std::string& task_name, std::size_t prio)
+#else
+             const std::string& task_name="", std::size_t prio=0)
+#endif
+{
+    auto l = [](const typename std::iterator_traits<Iterator1>::value_type& i,
+                const T& j)
+    {
+        return i == j;
+    };
+
+    return boost::asynchronous::top_level_callback_continuation_job<Iterator1,Job>
+            (boost::asynchronous::detail::parallel_search_n_helper<Iterator1,Size,T,decltype(l),Job>
+               (beg1,end1,count,value,std::move(l),cutoff,task_name,prio));
+}
+
+
+}}
+#endif // BOOST_ASYNCHRONOUS_PARALLEL_SEARCH_N_HPP
+
---
+++ libs/asynchronous/test/test_parallel_search_n.cpp
@@ -0,0 +1,318 @@
+// Boost.Asynchronous library
+//  Copyright (C) Christophe Henry 2015
+//
+//  Use, modification and distribution is subject to the Boost
+//  Software License, Version 1.0.  (See accompanying file
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// For more information, see http://www.boost.org
+
+#include <vector>
+#include <set>
+
+#include <boost/asynchronous/scheduler/single_thread_scheduler.hpp>
+#include <boost/asynchronous/queue/lockfree_queue.hpp>
+#include <boost/asynchronous/scheduler_shared_proxy.hpp>
+#include <boost/asynchronous/scheduler/threadpool_scheduler.hpp>
+
+#include <boost/asynchronous/servant_proxy.hpp>
+#include <boost/asynchronous/post.hpp>
+#include <boost/asynchronous/trackable_servant.hpp>
+#include <boost/asynchronous/algorithm/parallel_search_n.hpp>
+
+#include "test_common.hpp"
+
+#include <boost/test/unit_test.hpp>
+using namespace boost::asynchronous::test;
+
+namespace
+{
+// main thread id
+boost::thread::id main_thread_id;
+bool servant_dtor=false;
+void generate(std::vector<int>& data)
+{
+    data = std::vector<int>(10000,1);
+    std::mt19937 mt(static_cast<unsigned int>(std::time(nullptr)));
+    std::uniform_int_distribution<> dis(0, 1000);
+    std::generate(data.begin(), data.end(), std::bind(dis, std::ref(mt)));
+}
+
+struct Servant : boost::asynchronous::trackable_servant<>
+{
+    typedef std::vector<int>::iterator Iterator;
+    Servant(boost::asynchronous::any_weak_scheduler<> scheduler)
+        : boost::asynchronous::trackable_servant<>(scheduler,
+                                               boost::asynchronous::make_shared_scheduler_proxy<
+                                                   boost::asynchronous::threadpool_scheduler<
+                                                           boost::asynchronous::lockfree_queue<>>>(6))
+    {
+        generate(m_data);
+    }
+    ~Servant()
+    {
+        BOOST_CHECK_MESSAGE(main_thread_id!=boost::this_thread::get_id(),"servant dtor not posted.");
+        servant_dtor = true;
+    }
+
+    boost::shared_future<void> start_async_work_ok()
+    {
+        BOOST_CHECK_MESSAGE(main_thread_id!=boost::this_thread::get_id(),"servant start_async_work_ok not posted.");
+        m_data[3100]=42;
+        m_data[3101]=42;
+        m_data[3102]=42;
+        // we need a promise to inform caller when we're done
+        boost::shared_ptr<boost::promise<void> > aPromise(new boost::promise<void>);
+        boost::shared_future<void> fu = aPromise->get_future();
+        boost::asynchronous::any_shared_scheduler_proxy<> tp =get_worker();
+        std::vector<boost::thread::id> ids = tp.thread_ids();
+        // start long tasks
+        post_callback(
+           [ids,this](){
+                    BOOST_CHECK_MESSAGE(main_thread_id!=boost::this_thread::get_id(),"servant work not posted.");
+
+                    BOOST_CHECK_MESSAGE(contains_id(ids.begin(),ids.end(),boost::this_thread::get_id()),"task executed in the wrong thread");
+                    return boost::asynchronous::parallel_search_n(this->m_data.begin(),this->m_data.end(),
+                                                                3,42,
+                                                                [](int i, int j){return i == j;},1500);
+                    },// work
+           [aPromise,ids,this](boost::asynchronous::expected<Iterator> res){
+                        BOOST_CHECK_MESSAGE(!res.has_exception(),"servant work threw an exception.");
+                        BOOST_CHECK_MESSAGE(main_thread_id!=boost::this_thread::get_id(),"servant callback in main thread.");
+                        BOOST_CHECK_MESSAGE(!contains_id(ids.begin(),ids.end(),boost::this_thread::get_id()),"task callback executed in the wrong thread(pool)");
+                        BOOST_CHECK_MESSAGE(!res.has_exception(),"servant work threw an exception.");
+
+                        auto it = std::search_n(this->m_data.begin(),this->m_data.end(),
+                                                3,42,
+                                                [](int i, int j){return i == j;});
+
+                        BOOST_CHECK_MESSAGE(it != m_data.end(),"parallel_search_n should have found value.");
+                        BOOST_CHECK_MESSAGE(it == res.get(),"parallel_search_n found wrong value.");
+                        aPromise->set_value();
+           }// callback functor.
+        );
+        return fu;
+    }
+    boost::shared_future<void> start_async_work_nok()
+    {
+        BOOST_CHECK_MESSAGE(main_thread_id!=boost::this_thread::get_id(),"servant start_async_work_nok not posted.");
+        // we need a promise to inform caller when we're done
+        boost::shared_ptr<boost::promise<void> > aPromise(new boost::promise<void>);
+        boost::shared_future<void> fu = aPromise->get_future();
+        boost::asynchronous::any_shared_scheduler_proxy<> tp =get_worker();
+        std::vector<boost::thread::id> ids = tp.thread_ids();
+        // start long tasks
+        post_callback(
+           [ids,this](){
+                    BOOST_CHECK_MESSAGE(main_thread_id!=boost::this_thread::get_id(),"servant work not posted.");
+
+                    BOOST_CHECK_MESSAGE(contains_id(ids.begin(),ids.end(),boost::this_thread::get_id()),"task executed in the wrong thread");
+                    return boost::asynchronous::parallel_search_n(this->m_data.begin(),this->m_data.end(),
+                                                                  3,-1,
+                                                                  [](int i, int j){return i == j;},1500);
+                    },// work
+           [aPromise,ids,this](boost::asynchronous::expected<Iterator> res){
+                        BOOST_CHECK_MESSAGE(!res.has_exception(),"servant work threw an exception.");
+                        BOOST_CHECK_MESSAGE(main_thread_id!=boost::this_thread::get_id(),"servant callback in main thread.");
+                        BOOST_CHECK_MESSAGE(!contains_id(ids.begin(),ids.end(),boost::this_thread::get_id()),"task callback executed in the wrong thread(pool)");
+                        BOOST_CHECK_MESSAGE(!res.has_exception(),"servant work threw an exception.");
+
+                        auto it = std::search_n(this->m_data.begin(),this->m_data.end(),
+                                                3,-1,
+                                                [](int i, int j){return i == j;});
+
+                        BOOST_CHECK_MESSAGE(it == m_data.end(),"parallel_search_n should not have found value.");
+                        BOOST_CHECK_MESSAGE(it == res.get(),"parallel_search_n found wrong value.");
+                        aPromise->set_value();
+           }// callback functor.
+        );
+        return fu;
+    }
+
+    boost::shared_future<void> start_async_work_ok_no_fct()
+    {
+        BOOST_CHECK_MESSAGE(main_thread_id!=boost::this_thread::get_id(),"servant start_async_work_ok not posted.");
+        m_data[3100]=42;
+        m_data[3101]=42;
+        m_data[3102]=42;
+        // we need a promise to inform caller when we're done
+        boost::shared_ptr<boost::promise<void> > aPromise(new boost::promise<void>);
+        boost::shared_future<void> fu = aPromise->get_future();
+        boost::asynchronous::any_shared_scheduler_proxy<> tp =get_worker();
+        std::vector<boost::thread::id> ids = tp.thread_ids();
+        // start long tasks
+        post_callback(
+           [ids,this](){
+                    BOOST_CHECK_MESSAGE(main_thread_id!=boost::this_thread::get_id(),"servant work not posted.");
+
+                    BOOST_CHECK_MESSAGE(contains_id(ids.begin(),ids.end(),boost::this_thread::get_id()),"task executed in the wrong thread");
+                    return boost::asynchronous::parallel_search_n(this->m_data.begin(),this->m_data.end(),
+                                                                  3,42,1500);
+                    },// work
+           [aPromise,ids,this](boost::asynchronous::expected<Iterator> res){
+                        BOOST_CHECK_MESSAGE(!res.has_exception(),"servant work threw an exception.");
+                        BOOST_CHECK_MESSAGE(main_thread_id!=boost::this_thread::get_id(),"servant callback in main thread.");
+                        BOOST_CHECK_MESSAGE(!contains_id(ids.begin(),ids.end(),boost::this_thread::get_id()),"task callback executed in the wrong thread(pool)");
+                        BOOST_CHECK_MESSAGE(!res.has_exception(),"servant work threw an exception.");
+
+                        auto it = std::search_n(this->m_data.begin(),this->m_data.end(),3,42);
+
+                        BOOST_CHECK_MESSAGE(it != m_data.end(),"parallel_search_n should have found value.");
+                        BOOST_CHECK_MESSAGE(it == res.get(),"parallel_search_n found wrong value.");
+                        aPromise->set_value();
+           }// callback functor.
+        );
+        return fu;
+    }
+
+    boost::shared_future<void> start_async_work_ok2()
+    {
+        BOOST_CHECK_MESSAGE(main_thread_id!=boost::this_thread::get_id(),"servant start_async_work_ok not posted.");
+        m_data[2999]=42;
+        m_data[3000]=42;
+        m_data[3001]=42;
+        // we need a promise to inform caller when we're done
+        boost::shared_ptr<boost::promise<void> > aPromise(new boost::promise<void>);
+        boost::shared_future<void> fu = aPromise->get_future();
+        boost::asynchronous::any_shared_scheduler_proxy<> tp =get_worker();
+        std::vector<boost::thread::id> ids = tp.thread_ids();
+        // start long tasks
+        post_callback(
+           [ids,this](){
+                    BOOST_CHECK_MESSAGE(main_thread_id!=boost::this_thread::get_id(),"servant work not posted.");
+
+                    BOOST_CHECK_MESSAGE(contains_id(ids.begin(),ids.end(),boost::this_thread::get_id()),"task executed in the wrong thread");
+                    return boost::asynchronous::parallel_search_n(this->m_data.begin(),this->m_data.end(),
+                                                                  3,42,
+                                                                  [](int i, int j){return i == j;},1500);
+                    },// work
+           [aPromise,ids,this](boost::asynchronous::expected<Iterator> res){
+                        BOOST_CHECK_MESSAGE(!res.has_exception(),"servant work threw an exception.");
+                        BOOST_CHECK_MESSAGE(main_thread_id!=boost::this_thread::get_id(),"servant callback in main thread.");
+                        BOOST_CHECK_MESSAGE(!contains_id(ids.begin(),ids.end(),boost::this_thread::get_id()),"task callback executed in the wrong thread(pool)");
+                        BOOST_CHECK_MESSAGE(!res.has_exception(),"servant work threw an exception.");
+
+                        auto it = std::search_n(this->m_data.begin(),this->m_data.end(),
+                                                3,42,
+                                                [](int i, int j){return i == j;});
+
+                        BOOST_CHECK_MESSAGE(it != m_data.end(),"parallel_search_n should have found value.");
+                        BOOST_CHECK_MESSAGE(it == res.get(),"parallel_search_n found wrong value.");
+                        aPromise->set_value();
+           }// callback functor.
+        );
+        return fu;
+    }
+
+private:
+    std::vector<int> m_data;
+};
+
+class ServantProxy : public boost::asynchronous::servant_proxy<ServantProxy,Servant>
+{
+public:
+    template <class Scheduler>
+    ServantProxy(Scheduler s):
+        boost::asynchronous::servant_proxy<ServantProxy,Servant>(s)
+    {}
+    BOOST_ASYNC_FUTURE_MEMBER(start_async_work_ok)
+    BOOST_ASYNC_FUTURE_MEMBER(start_async_work_nok)
+    BOOST_ASYNC_FUTURE_MEMBER(start_async_work_ok2)
+    BOOST_ASYNC_FUTURE_MEMBER(start_async_work_ok_no_fct)
+};
+
+}
+
+BOOST_AUTO_TEST_CASE( test_parallel_search_n_ok )
+{
+    servant_dtor=false;
+    {
+        auto scheduler = boost::asynchronous::make_shared_scheduler_proxy<boost::asynchronous::single_thread_scheduler<
+                                                                            boost::asynchronous::lockfree_queue<>>>();
+
+        main_thread_id = boost::this_thread::get_id();
+        ServantProxy proxy(scheduler);
+        boost::shared_future<boost::shared_future<void> > fuv = proxy.start_async_work_ok();
+        try
+        {
+            boost::shared_future<void> resfuv = fuv.get();
+            resfuv.get();
+        }
+        catch(...)
+        {
+            BOOST_FAIL( "unexpected exception" );
+        }
+    }
+    BOOST_CHECK_MESSAGE(servant_dtor,"servant dtor not called.");
+}
+
+BOOST_AUTO_TEST_CASE( test_parallel_search_n_nok )
+{
+    servant_dtor=false;
+    {
+        auto scheduler = boost::asynchronous::make_shared_scheduler_proxy<boost::asynchronous::single_thread_scheduler<
+                                                                            boost::asynchronous::lockfree_queue<>>>();
+
+        main_thread_id = boost::this_thread::get_id();
+        ServantProxy proxy(scheduler);
+        boost::shared_future<boost::shared_future<void> > fuv = proxy.start_async_work_nok();
+        try
+        {
+            boost::shared_future<void> resfuv = fuv.get();
+            resfuv.get();
+        }
+        catch(...)
+        {
+            BOOST_FAIL( "unexpected exception" );
+        }
+    }
+    BOOST_CHECK_MESSAGE(servant_dtor,"servant dtor not called.");
+}
+
+BOOST_AUTO_TEST_CASE( test_parallel_search_n_ok2 )
+{
+    servant_dtor=false;
+    {
+        auto scheduler = boost::asynchronous::make_shared_scheduler_proxy<boost::asynchronous::single_thread_scheduler<
+                                                                            boost::asynchronous::lockfree_queue<>>>();
+
+        main_thread_id = boost::this_thread::get_id();
+        ServantProxy proxy(scheduler);
+        boost::shared_future<boost::shared_future<void> > fuv = proxy.start_async_work_ok2();
+        try
+        {
+            boost::shared_future<void> resfuv = fuv.get();
+            resfuv.get();
+        }
+        catch(...)
+        {
+            BOOST_FAIL( "unexpected exception" );
+        }
+    }
+    BOOST_CHECK_MESSAGE(servant_dtor,"servant dtor not called.");
+}
+
+BOOST_AUTO_TEST_CASE( test_parallel_search_n_ok_no_fct )
+{
+    servant_dtor=false;
+    {
+        auto scheduler = boost::asynchronous::make_shared_scheduler_proxy<boost::asynchronous::single_thread_scheduler<
+                                                                            boost::asynchronous::lockfree_queue<>>>();
+
+        main_thread_id = boost::this_thread::get_id();
+        ServantProxy proxy(scheduler);
+        boost::shared_future<boost::shared_future<void> > fuv = proxy.start_async_work_ok_no_fct();
+        try
+        {
+            boost::shared_future<void> resfuv = fuv.get();
+            resfuv.get();
+        }
+        catch(...)
+        {
+            BOOST_FAIL( "unexpected exception" );
+        }
+    }
+    BOOST_CHECK_MESSAGE(servant_dtor,"servant dtor not called.");
+}
+
+
