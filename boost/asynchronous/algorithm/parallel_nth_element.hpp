e5273aa3e0c42e4725ca7cb67a24e0b676ed932a
henry-ch <christophe.j.henry@googlemail.com>
2015-06-06 21:51:26 +0200

    added first version of parallel_nth_element

---
+++ boost/asynchronous/algorithm/parallel_nth_element.hpp
@@ -0,0 +1,159 @@
+// Boost.Asynchronous library
+//  Copyright (C) Christophe Henry 2015
+//
+//  Use, modification and distribution is subject to the Boost
+//  Software License, Version 1.0.  (See accompanying file
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// For more information, see http://www.boost.org
+
+#ifndef BOOST_ASYNCHRONOUS_PARALLEL_NTH_ELEMENT_HPP
+#define BOOST_ASYNCHRONOUS_PARALLEL_NTH_ELEMENT_HPP
+
+#include <algorithm>
+#include <vector>
+
+#include <boost/utility/enable_if.hpp>
+#include <boost/asynchronous/callable_any.hpp>
+#include <boost/asynchronous/continuation_task.hpp>
+#include <boost/asynchronous/post.hpp>
+#include <boost/asynchronous/algorithm/detail/safe_advance.hpp>
+#include <boost/asynchronous/detail/metafunctions.hpp>
+#include <boost/asynchronous/algorithm/parallel_stable_partition.hpp>
+#include <boost/asynchronous/algorithm/parallel_partition.hpp>
+
+namespace boost { namespace asynchronous
+{
+namespace detail
+{
+template <class Iterator, class Func, class Job>
+struct parallel_nth_element_helper: public boost::asynchronous::continuation_task<void>
+{
+    parallel_nth_element_helper(Iterator beg, Iterator end, Iterator nth, Func func,long cutoff, const uint32_t thread_num,
+                                const std::string& task_name, std::size_t prio)
+        : boost::asynchronous::continuation_task<void>(task_name),
+          beg_(beg),end_(end),nth_(nth),func_(std::move(func)),cutoff_(cutoff),thread_num_(thread_num),prio_(prio)
+    {}
+    void operator()()const
+    {
+        boost::asynchronous::continuation_result<void> task_res = this_task_result();
+        // advance up to cutoff
+        Iterator it = boost::asynchronous::detail::find_cutoff(beg_,cutoff_,end_);
+        // if not at end, recurse, otherwise execute here
+        if (it == end_)
+        {
+            std::nth_element(beg_,nth_,end_,func_);
+            task_res.set_value();
+        }
+        else
+        {
+            auto func(std::move(func_));
+            auto cutoff = cutoff_;
+            auto task_name = this->get_name();
+            auto prio = prio_;
+            auto thread_num = thread_num_;
+            auto beg = beg_;
+            auto end = end_;
+            auto nth=nth_;
+            auto nthval = *nth;
+            //std::cout << "*nth: " << nthval << std::endl;
+            auto l = [nthval](const typename std::iterator_traits<Iterator>::value_type& i)
+            {
+                return i < nthval;
+            };
+
+            //std::cout << "start parallel_partition: " << end_ - beg_ << std::endl;
+            boost::shared_ptr<std::vector<int>> temp = boost::make_shared<std::vector<int>>(beg_,end_);
+            auto cont = boost::asynchronous::parallel_stable_partition<Iterator,Iterator,decltype(l),Job>(beg_,end_,temp->begin(),std::move(l),cutoff_);
+            //auto cont = boost::asynchronous::parallel_partition<Iterator,decltype(l),Job>(beg_,end_,std::move(l),thread_num_);
+            cont.on_done([temp,task_res,beg,end,nth,func,cutoff,thread_num,task_name,prio]
+                         (std::tuple<boost::asynchronous::expected<Iterator> >&& continuation_res)
+            {
+                try
+                {
+                    auto res = std::move(std::get<0>(continuation_res).get());
+                    auto d = std::distance(temp->begin(),res);
+                    std::swap_ranges(beg,end,temp->begin());
+                    //auto d = std::distance(beg,res);
+                    res = beg+d;
+                    //std::cout << "done parallel_partition: " << res-beg << " , " << nth-beg  << std::endl;
+                    if (std::distance(beg,res) >= std::distance(beg,nth))
+                    {
+                        // re-iterate on first part
+                        //std::cout << "1st part: " << res-beg << std::endl;
+                        auto cont = boost::asynchronous::top_level_callback_continuation_job<void,Job>
+                                (boost::asynchronous::detail::parallel_nth_element_helper<Iterator,Func,Job>
+                                 (beg,res,nth,std::move(func),cutoff,thread_num,task_name,prio));
+                        cont.on_done([task_res](std::tuple<boost::asynchronous::expected<void> >&& nth_element_res)
+                        {
+                            try
+                            {
+                                // check for exceptions
+                                std::get<0>(nth_element_res).get();
+                                task_res.set_value();
+                            }
+                            catch(std::exception& e)
+                            {
+                                task_res.set_exception(boost::copy_exception(e));
+                            }
+                        });
+                    }
+                    else
+                    {
+                        // re-iterate on second part
+                        //std::cout << "2nd part: " << end-res << std::endl;
+                        auto cont = boost::asynchronous::top_level_callback_continuation_job<void,Job>
+                                (boost::asynchronous::detail::parallel_nth_element_helper<Iterator,Func,Job>
+                                 (res,end,nth,std::move(func),cutoff,thread_num,task_name,prio));
+                        cont.on_done([task_res](std::tuple<boost::asynchronous::expected<void> >&& nth_element_res)
+                        {
+                            try
+                            {
+                                // check for exceptions
+                                std::get<0>(nth_element_res).get();
+                                task_res.set_value();
+                            }
+                            catch(std::exception& e)
+                            {
+                                task_res.set_exception(boost::copy_exception(e));
+                            }
+                        });
+                    }
+
+                }
+                catch(std::exception& e)
+                {
+                    task_res.set_exception(boost::copy_exception(e));
+                }
+            });
+        }
+
+    }
+    Iterator beg_;
+    Iterator end_;
+    Iterator nth_;
+    Func func_;
+    long cutoff_;
+    uint32_t thread_num_;
+    std::size_t prio_;
+};
+}
+
+template <class Iterator,class Func, class Job=BOOST_ASYNCHRONOUS_DEFAULT_JOB>
+boost::asynchronous::detail::callback_continuation<void,Job>
+parallel_nth_element(Iterator beg, Iterator nth, Iterator end, Func func,long cutoff,const uint32_t thread_num = 1,
+                   #ifdef BOOST_ASYNCHRONOUS_REQUIRE_ALL_ARGUMENTS
+                   const std::string& task_name, std::size_t prio)
+#else
+                   const std::string& task_name="", std::size_t prio =0)
+#endif
+{
+    return boost::asynchronous::top_level_callback_continuation_job<void,Job>
+            (boost::asynchronous::detail::parallel_nth_element_helper<Iterator,Func,Job>
+             (beg,end,nth,std::move(func),cutoff,thread_num,task_name,prio));
+
+}
+
+}}
+#endif // BOOST_ASYNCHRONOUS_PARALLEL_NTH_ELEMENT_HPP
+
---
+++ libs/asynchronous/test/test_parallel_nth_element.cpp
@@ -0,0 +1,157 @@
+// Boost.Asynchronous library
+//  Copyright (C) Christophe Henry 2013
+//
+//  Use, modification and distribution is subject to the Boost
+//  Software License, Version 1.0.  (See accompanying file
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// For more information, see http://www.boost.org
+
+#include <vector>
+#include <set>
+#include <functional>
+#include <random>
+#include <boost/lexical_cast.hpp>
+
+#include <boost/asynchronous/scheduler/single_thread_scheduler.hpp>
+#include <boost/asynchronous/queue/lockfree_queue.hpp>
+#include <boost/asynchronous/scheduler_shared_proxy.hpp>
+#include <boost/asynchronous/scheduler/threadpool_scheduler.hpp>
+
+#include <boost/asynchronous/servant_proxy.hpp>
+#include <boost/asynchronous/post.hpp>
+#include <boost/asynchronous/trackable_servant.hpp>
+#include <boost/asynchronous/algorithm/parallel_nth_element.hpp>
+
+#include "test_common.hpp"
+
+#include <boost/test/unit_test.hpp>
+using namespace boost::asynchronous::test;
+
+namespace
+{
+// main thread id
+boost::thread::id main_thread_id;
+bool servant_dtor=false;
+typedef std::vector<int>::iterator Iterator;
+typedef std::vector<int>::const_iterator ConstIterator;
+
+struct my_exception : virtual boost::exception, virtual std::exception
+{
+    virtual const char* what() const throw()
+    {
+        return "my_exception";
+    }
+};
+void generate(std::vector<int>& data, unsigned elements, unsigned dist)
+{
+    data = std::vector<int>(elements,1);
+    std::random_device rd;
+    std::mt19937 mt(rd());
+    //std::mt19937 mt(static_cast<unsigned int>(std::time(nullptr)));
+    std::uniform_int_distribution<> dis(0, dist);
+    std::generate(data.begin(), data.end(), std::bind(dis, std::ref(mt)));
+}
+
+struct Servant : boost::asynchronous::trackable_servant<>
+{
+    typedef int simple_ctor;
+    Servant(boost::asynchronous::any_weak_scheduler<> scheduler)
+        : boost::asynchronous::trackable_servant<>(scheduler,
+                                               boost::asynchronous::create_shared_scheduler_proxy(
+                                                   new boost::asynchronous::threadpool_scheduler<
+                                                           boost::asynchronous::lockfree_queue<> >(boost::thread::hardware_concurrency())))
+    {
+    }
+    ~Servant()
+    {
+        BOOST_CHECK_MESSAGE(main_thread_id!=boost::this_thread::get_id(),"servant dtor not posted.");
+        servant_dtor = true;
+    }
+
+    boost::shared_future<void> test_parallel_nth_element()
+    {
+        BOOST_CHECK_MESSAGE(main_thread_id!=boost::this_thread::get_id(),"servant async work not posted.");
+        generate(m_data1,10000,9000);
+        /*for (auto i = 0 ; i < 9999; ++i)
+        {
+            m_data1.push_back(i);
+        }
+        std::random_shuffle(m_data1.begin(),m_data1.end());*/
+        auto data_copy = m_data1;
+        // we need a promise to inform caller when we're done
+        boost::shared_ptr<boost::promise<void> > aPromise(new boost::promise<void>);
+        boost::shared_future<void> fu = aPromise->get_future();
+        boost::asynchronous::any_shared_scheduler_proxy<> tp =get_worker();
+        auto itnth = m_data1.begin()+2000;
+
+        // start long tasks
+        post_callback(
+           [tp,this,itnth](){
+                    BOOST_CHECK_MESSAGE(main_thread_id!=boost::this_thread::get_id(),"servant work not posted.");
+                    std::vector<boost::thread::id> ids = tp.thread_ids();
+                    BOOST_CHECK_MESSAGE(contains_id(ids.begin(),ids.end(),boost::this_thread::get_id()),"task executed in the wrong thread");
+                    return boost::asynchronous::parallel_nth_element(m_data1.begin(),itnth,m_data1.end(),
+                                                                     [](int i,int j){return i < j;},
+                                                                     1000,
+                                                                     boost::thread::hardware_concurrency()
+                    );
+                    },// work
+           [aPromise,tp,data_copy,this,itnth](boost::asynchronous::expected<void> res) mutable{
+                        BOOST_CHECK_MESSAGE(!res.has_exception(),"servant work threw an exception.");
+                        BOOST_CHECK_MESSAGE(main_thread_id!=boost::this_thread::get_id(),"servant callback in main thread.");
+                        std::vector<boost::thread::id> ids = tp.thread_ids();
+                        BOOST_CHECK_MESSAGE(!contains_id(ids.begin(),ids.end(),boost::this_thread::get_id()),"task callback executed in the wrong thread(pool)");
+                        BOOST_CHECK_MESSAGE(!res.has_exception(),"servant work threw an exception.");
+
+                        auto itnth2 = data_copy.begin()+2000;
+                        std::nth_element(data_copy.begin(),itnth2,data_copy.end(),
+                                         [](int i,int j){return i < j;});
+
+                        BOOST_CHECK_MESSAGE(*itnth2 == *itnth,"parallel_nth_element gave the wrong result.");
+                        aPromise->set_value();
+           }// callback functor.
+        );
+        return fu;
+    }
+
+private:
+    std::vector<int> m_data1;
+};
+class ServantProxy : public boost::asynchronous::servant_proxy<ServantProxy,Servant>
+{
+public:
+    template <class Scheduler>
+    ServantProxy(Scheduler s):
+        boost::asynchronous::servant_proxy<ServantProxy,Servant>(s)
+    {}
+    BOOST_ASYNC_FUTURE_MEMBER(test_parallel_nth_element)
+
+};
+
+}
+
+BOOST_AUTO_TEST_CASE( test_parallel_nth_element )
+{
+    servant_dtor=false;
+    {
+        auto scheduler = boost::asynchronous::create_shared_scheduler_proxy(new boost::asynchronous::single_thread_scheduler<
+                                                                            boost::asynchronous::lockfree_queue<> >);
+
+        main_thread_id = boost::this_thread::get_id();
+        ServantProxy proxy(scheduler);
+        boost::shared_future<boost::shared_future<void> > fuv = proxy.test_parallel_nth_element();
+        try
+        {
+            boost::shared_future<void> resfuv = fuv.get();
+            resfuv.get();
+        }
+        catch(...)
+        {
+            BOOST_FAIL( "unexpected exception" );
+        }
+    }
+    BOOST_CHECK_MESSAGE(servant_dtor,"servant dtor not called.");
+}
+
+
