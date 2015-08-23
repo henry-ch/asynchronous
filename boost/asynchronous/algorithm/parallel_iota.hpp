fe644ac3cfbba13a2ef70e000a7c44132a0a0b61
henry-ch <christophe.j.henry@googlemail.com>
2015-08-23 11:17:18 +0200

    added parallel_iota

---
+++ boost/asynchronous/algorithm/parallel_iota.hpp
@@ -0,0 +1,188 @@
+// Boost.Asynchronous library
+//  Copyright (C) Tobias Holl 2015
+//
+//  Use, modification and distribution is subject to the Boost
+//  Software License, Version 1.0.  (See accompanying file
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// For more information, see http://www.boost.org
+
+#ifndef BOOST_ASYNCHRON_PARALLEL_IOTA_HPP
+#define BOOST_ASYNCHRON_PARALLEL_IOTA_HPP
+
+#include <algorithm>
+
+#include <boost/asynchronous/detail/continuation_impl.hpp>
+#include <boost/asynchronous/continuation_task.hpp>
+#include <boost/asynchronous/algorithm/detail/safe_advance.hpp>
+
+#include <boost/range/begin.hpp>
+#include <boost/range/end.hpp>
+#include <boost/range/has_range_iterator.hpp>
+
+namespace boost { namespace asynchronous {
+
+namespace detail {
+
+template <typename Iterator>
+struct iterator_value_type
+{
+    using type = typename std::remove_reference<typename std::remove_cv<decltype(*std::declval<Iterator>())>::type>::type;
+};
+
+template <typename Range>
+struct range_value_type
+{
+    using type = typename std::remove_reference<typename std::remove_cv<decltype(*boost::begin(std::declval<Range>()))>::type>::type;
+};
+
+template <typename Iterator, typename Job=BOOST_ASYNCHRONOUS_DEFAULT_JOB>
+struct parallel_iota_inplace_helper : public boost::asynchronous::continuation_task<void>
+{
+    using value_type = typename boost::asynchronous::detail::iterator_value_type<Iterator>::type;
+
+    parallel_iota_inplace_helper(Iterator beg, Iterator end, value_type start, long cutoff,
+                                 std::string const& task_name, std::size_t prio)
+        : boost::asynchronous::continuation_task<void>(task_name)
+        , beg_(beg), end_(end), start_(start), cutoff_(cutoff), prio_(prio)
+    {}
+
+    void operator()() const
+    {
+        boost::asynchronous::continuation_result<void> task_res = this_task_result();
+        
+        // Advance iterator up to the cutoff
+        Iterator it = boost::asynchronous::detail::find_cutoff(beg_, cutoff_, end_);
+        // If the end is reached, use this thread to execute the subtask, otherwise recurse
+        if (it == end_)
+        {
+            std::iota(beg_, it, start_);
+            task_res.set_value();
+        }
+        else
+        {
+            auto distance = std::distance(beg_, it);
+            boost::asynchronous::create_callback_continuation_job<Job>(
+                        // Callback
+                        [task_res](std::tuple<boost::asynchronous::expected<void>, boost::asynchronous::expected<void> > res)
+                        {
+                            try
+                            {
+                                // Call get() to check for exceptions
+                                std::get<0>(res).get();
+                                std::get<1>(res).get();
+                                task_res.set_value();
+                            }
+                            catch (std::exception& e)
+                            {
+                                task_res.set_exception(boost::copy_exception(e));
+                            }
+                        },
+                        // recursive tasks
+                        parallel_iota_inplace_helper<Iterator, Job>(beg_, it, start_, cutoff_, this->get_name(), prio_),
+                        parallel_iota_inplace_helper<Iterator, Job>(it, end_, start_ + distance, cutoff_, this->get_name(), prio_)
+               );
+        }
+    }
+
+    Iterator beg_;
+    Iterator end_;
+    value_type start_;
+    long cutoff_;
+    std::size_t prio_;
+};
+
+}
+
+template <typename Iterator, typename Job=BOOST_ASYNCHRONOUS_DEFAULT_JOB>
+boost::asynchronous::detail::callback_continuation<void, Job>
+parallel_iota(Iterator begin, Iterator end, typename boost::asynchronous::detail::iterator_value_type<Iterator>::type start, long cutoff,
+#ifdef BOOST_ASYNCHRONOUS_REQUIRE_ALL_ARGUMENTS
+              const std::string& task_name, std::size_t prio)
+#else
+              const std::string& task_name="", std::size_t prio=0)
+#endif
+{
+    return boost::asynchronous::top_level_callback_continuation_job<void, Job>(boost::asynchronous::detail::parallel_iota_inplace_helper<Iterator, Job>(begin, end, start, cutoff, task_name, prio));
+}
+
+namespace detail {
+
+template <typename Range, typename Job=BOOST_ASYNCHRONOUS_DEFAULT_JOB>
+struct parallel_iota_generate_helper : public boost::asynchronous::continuation_task<Range>
+{
+    using value_type = typename boost::asynchronous::detail::range_value_type<Range>::type;
+
+    parallel_iota_generate_helper(boost::shared_ptr<Range> range, value_type start, long cutoff,
+                                 std::string const& task_name, std::size_t prio)
+        : boost::asynchronous::continuation_task<Range>(task_name)
+        , range_(range), start_(start), cutoff_(cutoff), prio_(prio)
+    {}
+
+    void operator()()
+    {
+        boost::shared_ptr<Range> range = std::move(range_);
+        boost::asynchronous::continuation_result<Range> task_res = this->this_task_result();
+        
+        // Get iterators
+        auto beg = boost::begin(*range);
+        auto end = boost::end(*range);
+
+        // Advance iterator up to the cutoff
+        auto it = boost::asynchronous::detail::find_cutoff(beg, cutoff_, end);
+        // If the end is reached, use this thread to execute the subtask, otherwise recurse
+        if (it == end)
+        {
+            std::iota(beg, it, start_);
+            task_res.set_value(std::move(*range));
+        }
+        else
+        {
+            auto distance = std::distance(beg, it);
+            boost::asynchronous::create_callback_continuation_job<Job>(
+                        // Callback
+                        [task_res, range](std::tuple<boost::asynchronous::expected<void>, boost::asynchronous::expected<void> > res)
+                        {
+                            try
+                            {
+                                // Call get() to check for exceptions
+                                std::get<0>(res).get();
+                                std::get<1>(res).get();
+                                task_res.set_value(std::move(*range));
+                            }
+                            catch (std::exception& e)
+                            {
+                                task_res.set_exception(boost::copy_exception(e));
+                            }
+                        },
+                        // recursive tasks
+                        parallel_iota_inplace_helper<decltype(it), Job>(beg, it, start_, cutoff_, this->get_name(), prio_),
+                        parallel_iota_inplace_helper<decltype(it), Job>(it, end, start_ + distance, cutoff_, this->get_name(), prio_)
+               );
+        }
+    }
+
+    boost::shared_ptr<Range> range_;
+    value_type start_;
+    long cutoff_;
+    std::size_t prio_;
+};
+
+}
+
+template <typename Range, typename Job=BOOST_ASYNCHRONOUS_DEFAULT_JOB>
+typename boost::enable_if<boost::has_range_iterator<Range>, boost::asynchronous::detail::callback_continuation<Range, Job>>::type
+parallel_iota(Range && range, typename boost::asynchronous::detail::range_value_type<Range>::type start, long cutoff,
+#ifdef BOOST_ASYNCHRONOUS_REQUIRE_ALL_ARGUMENTS
+              const std::string& task_name, std::size_t prio)
+#else
+              const std::string& task_name="", std::size_t prio=0)
+#endif
+{
+    auto range_ptr = boost::make_shared<Range>(std::forward<Range>(range));
+    return boost::asynchronous::top_level_callback_continuation_job<Range, Job>(boost::asynchronous::detail::parallel_iota_generate_helper<Range, Job>(range_ptr, start, cutoff, task_name, prio));
+}
+
+}}
+
+#endif // BOOST_ASYNCHRON_PARALLEL_IOTA_HPP
---
+++ libs/asynchronous/test/test_parallel_iota.cpp
@@ -0,0 +1,180 @@
+// Boost.Asynchronous library
+//  Copyright (C) Christophe Henry, Tobias Holl 2015
+//
+//  Use, modification and distribution is subject to the Boost
+//  Software License, Version 1.0.  (See accompanying file
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// For more information, see http://www.boost.org
+
+#include <vector>
+
+#include <boost/asynchronous/scheduler/single_thread_scheduler.hpp>
+#include <boost/asynchronous/queue/lockfree_queue.hpp>
+#include <boost/asynchronous/scheduler_shared_proxy.hpp>
+#include <boost/asynchronous/scheduler/threadpool_scheduler.hpp>
+
+#include <boost/asynchronous/servant_proxy.hpp>
+#include <boost/asynchronous/post.hpp>
+#include <boost/asynchronous/trackable_servant.hpp>
+#include <boost/asynchronous/algorithm/parallel_iota.hpp>
+
+#include "test_common.hpp"
+
+#include <boost/test/unit_test.hpp>
+
+using namespace boost::asynchronous::test;
+
+namespace
+{
+// main thread id
+boost::thread::id main_thread_id;
+bool servant_dtor=false;
+
+struct my_exception : virtual boost::exception, virtual std::exception
+{
+    virtual const char* what() const throw()
+    {
+        return "my_exception";
+    }
+};
+
+struct Servant : boost::asynchronous::trackable_servant<>
+{
+    typedef int simple_ctor;
+    Servant(boost::asynchronous::any_weak_scheduler<> scheduler)
+        : boost::asynchronous::trackable_servant<>(scheduler,
+                                               boost::asynchronous::make_shared_scheduler_proxy<
+                                                   boost::asynchronous::threadpool_scheduler<
+                                                           boost::asynchronous::lockfree_queue<>>>(6))
+        , m_data(10000, 0)
+    {
+    }
+    ~Servant()
+    {
+        BOOST_CHECK_MESSAGE(main_thread_id!=boost::this_thread::get_id(),"servant dtor not posted.");
+        servant_dtor = true;
+    }
+
+    boost::shared_future<void> start_async_work()
+    {
+        BOOST_CHECK_MESSAGE(main_thread_id!=boost::this_thread::get_id(),"servant start_async_work not posted.");
+        // we need a promise to inform caller when we're done
+        boost::shared_ptr<boost::promise<void> > aPromise(new boost::promise<void>);
+        boost::shared_future<void> fu = aPromise->get_future();
+        boost::asynchronous::any_shared_scheduler_proxy<> tp =get_worker();
+        std::vector<boost::thread::id> ids = tp.thread_ids();
+        // start long tasks
+        post_callback(
+           [ids,this](){
+                    BOOST_CHECK_MESSAGE(main_thread_id!=boost::this_thread::get_id(),"servant work not posted.");
+                    BOOST_CHECK_MESSAGE(contains_id(ids.begin(),ids.end(),boost::this_thread::get_id()),"task executed in the wrong thread");
+                    return boost::asynchronous::parallel_iota(this->m_data.begin(), this->m_data.end(), 0, 1500);
+                    },// work
+           [aPromise,ids,this](boost::asynchronous::expected<void> res){
+                        BOOST_CHECK_MESSAGE(!res.has_exception(),"servant work threw an exception.");
+                        BOOST_CHECK_MESSAGE(main_thread_id!=boost::this_thread::get_id(),"servant callback in main thread.");
+                        BOOST_CHECK_MESSAGE(!contains_id(ids.begin(),ids.end(),boost::this_thread::get_id()),"task callback executed in the wrong thread(pool)");
+                        for (std::size_t index = 0; index < this->m_data.size(); ++index) {
+                            BOOST_CHECK_MESSAGE((this->m_data[index] == (int)index), ("m_data[" + std::to_string(index) + "] is wrong: " + std::to_string(this->m_data[index])));
+                        }
+                        // reset
+                        m_data.clear();
+                        m_data.resize(10000, 0);
+                        aPromise->set_value();
+           }// callback functor.
+        );
+        return fu;
+    }
+    boost::shared_future<void> start_async_work_range()
+    {
+        BOOST_CHECK_MESSAGE(main_thread_id!=boost::this_thread::get_id(),"servant start_async_work_range not posted.");
+        // we need a promise to inform caller when we're done
+        boost::shared_ptr<boost::promise<void> > aPromise(new boost::promise<void>);
+        boost::shared_future<void> fu = aPromise->get_future();
+        boost::asynchronous::any_shared_scheduler_proxy<> tp =get_worker();
+        std::vector<boost::thread::id> ids = tp.thread_ids();
+        // start long tasks
+        post_callback(
+           [ids,this](){
+                    BOOST_CHECK_MESSAGE(main_thread_id!=boost::this_thread::get_id(),"servant work not posted.");
+                    BOOST_CHECK_MESSAGE(contains_id(ids.begin(),ids.end(),boost::this_thread::get_id()),"task executed in the wrong thread");
+                    return boost::asynchronous::parallel_iota(std::move(this->m_data), 0, 1500);
+                    },// work
+           [aPromise,ids,this](boost::asynchronous::expected<std::vector<int>> res){
+                        BOOST_CHECK_MESSAGE(!res.has_exception(),"servant work threw an exception.");
+                        BOOST_CHECK_MESSAGE(main_thread_id!=boost::this_thread::get_id(),"servant callback in main thread.");
+                        BOOST_CHECK_MESSAGE(!contains_id(ids.begin(),ids.end(),boost::this_thread::get_id()),"task callback executed in the wrong thread(pool)");
+                        std::vector<int> r = res.get();
+                        for (std::size_t index = 0; index < r.size(); ++index) {
+                            BOOST_CHECK_MESSAGE((r[index] == index), ("r[" + std::to_string(index) + "] is wrong: " + std::to_string(r[index])));
+                        }
+                        // reset
+                        m_data = std::vector<int>(10000, 0);
+                        aPromise->set_value();
+           }// callback functor.
+        );
+        return fu;
+    }
+private:
+    std::vector<int> m_data;
+};
+
+class ServantProxy : public boost::asynchronous::servant_proxy<ServantProxy,Servant>
+{
+public:
+    template <class Scheduler>
+    ServantProxy(Scheduler s):
+        boost::asynchronous::servant_proxy<ServantProxy,Servant>(s)
+    {}
+    BOOST_ASYNC_FUTURE_MEMBER(start_async_work)
+    BOOST_ASYNC_FUTURE_MEMBER(start_async_work_range)
+};
+
+}
+
+BOOST_AUTO_TEST_CASE( test_parallel_iota )
+{
+    servant_dtor=false;
+    {
+        auto scheduler = boost::asynchronous::make_shared_scheduler_proxy<boost::asynchronous::single_thread_scheduler<
+                                                                            boost::asynchronous::lockfree_queue<>>>();
+        main_thread_id = boost::this_thread::get_id();
+        ServantProxy proxy(scheduler);
+        boost::shared_future<boost::shared_future<void> > fuv = proxy.start_async_work();
+        try
+        {
+            boost::shared_future<void> resfuv = fuv.get();
+            resfuv.get();
+        }
+        catch(...)
+        {
+            BOOST_FAIL( "unexpected exception" );
+        }
+    }
+    BOOST_CHECK_MESSAGE(servant_dtor,"servant dtor not called.");
+}
+
+BOOST_AUTO_TEST_CASE( test_parallel_iota_range )
+{
+    servant_dtor=false;
+    {
+        auto scheduler = boost::asynchronous::make_shared_scheduler_proxy<boost::asynchronous::single_thread_scheduler<
+                                                                            boost::asynchronous::lockfree_queue<>>>();
+
+        main_thread_id = boost::this_thread::get_id();
+        ServantProxy proxy(scheduler);
+        boost::shared_future<boost::shared_future<void> > fuv = proxy.start_async_work_range();
+        try
+        {
+            boost::shared_future<void> resfuv = fuv.get();
+            resfuv.get();
+        }
+        catch(...)
+        {
+            BOOST_FAIL( "unexpected exception" );
+        }
+    }
+    BOOST_CHECK_MESSAGE(servant_dtor,"servant dtor not called.");
+}
+
