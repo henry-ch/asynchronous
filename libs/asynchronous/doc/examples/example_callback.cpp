589b4f79078954de78a95fc942b931de8b4ed124
henry-ch <christophe.j.henry@googlemail.com>
2016-12-01 21:24:03 +0100

    added example

--- libs/asynchronous/doc/examples/example_callback.cpp
+++ libs/asynchronous/doc/examples/example_callback.cpp
@@ -1,11 +1,8 @@
 #include <iostream>
 
 #include <boost/asynchronous/scheduler/single_thread_scheduler.hpp>
-#include <boost/asynchronous/queue/lockfree_stack.hpp>
-#include <boost/asynchronous/queue/lockfree_queue.hpp>
 #include <boost/asynchronous/scheduler_shared_proxy.hpp>
 #include <boost/asynchronous/scheduler/multiqueue_threadpool_scheduler.hpp>
-#include <boost/asynchronous/continuation_task.hpp>
 
 #include <boost/asynchronous/servant_proxy.hpp>
 #include <boost/asynchronous/trackable_servant.hpp>
@@ -14,133 +11,110 @@ using namespace std;
 
 namespace
 {
-// a simple, single-threaded fibonacci function used for cutoff
-long serial_fib( long n ) {
-    if( n<2 )
-        return n;
-    else
-        return serial_fib(n-1)+serial_fib(n-2);
-}
-
-// our recursive fibonacci tasks. Needs to inherit continuation_task<value type returned by this task>
-struct fib_task : public boost::asynchronous::continuation_task<long>
+struct Manager : boost::asynchronous::trackable_servant<>
 {
-    fib_task(long n,long cutoff):n_(n),cutoff_(cutoff){}
-    void operator()()const
-    {
-        // the result of this task, will be either set directly if < cutoff, otherwise when taks is ready
-        boost::asynchronous::continuation_result<long> task_res = this_task_result();
-        if (n_<cutoff_)
-        {
-            // n < cutoff => execute ourselves
-            task_res.set_value(serial_fib(n_));
-        }
-        else
-        {
-            // n> cutoff, create 2 new tasks and when both are done, set our result (res(task1) + res(task2))
-            boost::asynchronous::create_continuation(
-                        // called when subtasks are done, set our result
-                        [task_res](std::tuple<boost::future<long>,boost::future<long> > res)
-                        {
-                            long r = std::get<0>(res).get() + std::get<1>(res).get();
-                            task_res.set_value(r);
-                        },
-                        // recursive tasks
-                        fib_task(n_-1,cutoff_),
-                        fib_task(n_-2,cutoff_));
-        }
-    }
-    long n_;
-    long cutoff_;
-};
-
-struct Servant : boost::asynchronous::trackable_servant<>
-{
-    // optional, ctor is simple enough not to be posted
-    typedef int simple_ctor;
-    Servant(boost::asynchronous::any_weak_scheduler<> scheduler, int threads)
+    Manager(boost::asynchronous::any_weak_scheduler<> scheduler, int threads)
         : boost::asynchronous::trackable_servant<>(scheduler,
                                                // threadpool and a simple lockfree_queue queue
                                                boost::asynchronous::make_shared_scheduler_proxy<
                                                    boost::asynchronous::multiqueue_threadpool_scheduler<
                                                            boost::asynchronous::lockfree_queue<>>>(threads))
-        // for testing purpose
-        , m_promise(new boost::promise<long>)
+        // to signal that we shutdown
+        , m_promise()
     {
     }
-    // called when task done, in our thread
-    void on_callback(long res)
+    void second_task()
     {
-        // inform test caller
-        m_promise->set_value(res);
+        // this will probably never be called as our example cancels immediately
+        post_callback(
+                []()
+                {
+                     cout << "execute second task" << endl;
+                     // simulate long task
+                     boost::this_thread::sleep(boost::posix_time::milliseconds(1000));
+                },
+                // callback
+                [this](boost::asynchronous::expected<void> /*res*/)
+                {
+                    // ignore expected as no exception will be thrown
+                    cout << "shutdown servant after completion" <<endl;
+                    m_promise.set_value();
+                }
+        );
     }
+    void cancel()
+    {
+        cout << "second part cancelled" << endl;
+        needs_second_task = false;
+    }
+
     // call to this is posted and executes in our (safe) single-thread scheduler
-    boost::shared_future<long> calc_fibonacci(long n,long cutoff)
+    boost::future<void> start()
     {
-        // for testing purpose
-        boost::shared_future<long> fu = m_promise->get_future();
+        cout << "post first task to threadpool" << endl;
+        // to inform main  of shutdown
+        boost::future<void> fu = m_promise.get_future();
         // start long tasks in threadpool (first lambda) and callback in our thread
         post_callback(
-                [n,cutoff]()
+                []()
                 {
-                     // a top-level continuation is the first one in a recursive serie.
-                     // Its result will be passed to callback
-                     return boost::asynchronous::top_level_continuation<long>(fib_task(n,cutoff));
-                 }// work
-               ,
-               // callback with fibonacci result.
-               [this](boost::asynchronous::expected<long> res){
-                            this->on_callback(res.get());
-               }// callback functor.
+                     cout << "execute first task" << endl;
+                     // simulate long task
+                     boost::this_thread::sleep(boost::posix_time::milliseconds(1000));
+                },
+                // callback
+                [this](boost::asynchronous::expected<void> /*res*/)
+                {
+                    // ignore expected as no exception will be thrown
+                    // check now, only after first call if we want to continue, based on whatever happened since we started a task
+                    if (needs_second_task)
+                    {
+                        second_task();
+                    }
+                    else
+                    {
+                        cout << "shutdown servant after first task" <<endl;
+                        m_promise.set_value();
+                    }
+                }
         );
         return fu;
     }
 private:
-// for testing
-boost::shared_ptr<boost::promise<long> > m_promise;
+// to signal that we shutdown
+boost::promise<void> m_promise;
+bool needs_second_task=true;
 };
-class ServantProxy : public boost::asynchronous::servant_proxy<ServantProxy,Servant>
+class ManagerProxy : public boost::asynchronous::servant_proxy<ManagerProxy,Manager>
 {
 public:
     template <class Scheduler>
-    ServantProxy(Scheduler s, int threads):
-        boost::asynchronous::servant_proxy<ServantProxy,Servant>(s,threads)
+    ManagerProxy(Scheduler s, int threads):
+        boost::asynchronous::servant_proxy<ManagerProxy,Manager>(s,threads)
     {}
     // caller will get a future
-    BOOST_ASYNC_FUTURE_MEMBER(calc_fibonacci)
+    BOOST_ASYNC_FUTURE_MEMBER(start)
+    BOOST_ASYNC_FUTURE_MEMBER(cancel)
 };
 
 }
 
-void example_fibonacci(long fibo_val,long cutoff, int threads)
+void example_callback()
 {
-    std::cout << "example_fibonacci single" << std::endl;
-    typename boost::chrono::high_resolution_clock::time_point start;
-    typename boost::chrono::high_resolution_clock::time_point stop;
-    start = boost::chrono::high_resolution_clock::now();
-    long sres = serial_fib(fibo_val);
-    stop = boost::chrono::high_resolution_clock::now();
-    std::cout << "sres= " << sres << std::endl;
-    std::cout << "example_fibonacci single took in us:"
-              <<  (boost::chrono::nanoseconds(stop - start).count() / 1000) <<"\n" <<std::endl;
-
-    std::cout << "example_fibonacci parallel" << std::endl;
+    std::cout << "example_callback" << std::endl;
     {
-        // a single-threaded world, where Servant will live.
+        // a single-threaded world, where Manager will live.
         auto scheduler = boost::asynchronous::make_shared_scheduler_proxy<
                                 boost::asynchronous::single_thread_scheduler<
                                      boost::asynchronous::lockfree_queue<>>>();
         {
-            ServantProxy proxy(scheduler,threads);
-            start = boost::chrono::high_resolution_clock::now();
-            boost::shared_future<boost::shared_future<long> > fu = proxy.calc_fibonacci(fibo_val,cutoff);
-            boost::shared_future<long> resfu = fu.get();
-            long res = resfu.get();
-            stop = boost::chrono::high_resolution_clock::now();
-            std::cout << "res= " << res << std::endl;
-            std::cout << "example_fibonacci parallel took in us:"
-                      <<  (boost::chrono::nanoseconds(stop - start).count() / 1000) <<"\n" <<std::endl;
+            ManagerProxy proxy(scheduler,boost::thread::hardware_concurrency());
+            boost::future<boost::future<void> > fu = proxy.start();
+            // we changed our mind, inform Manager by posting him a call to cancel()
+            proxy.cancel();
+            // wait for shutdown
+            fu.get().get();
         }
     }
-    std::cout << "end example_fibonacci \n" << std::endl;
+    std::cout << "end example_callback \n" << std::endl;
 }
