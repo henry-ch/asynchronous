3c2a0fefc4046be52b038e2541be37191dfc593f
henry-ch <christophe.j.henry@googlemail.com>
2017-04-03 21:09:26 +0200

    added doxygen comments

--- boost/asynchronous/trackable_servant.hpp
+++ boost/asynchronous/trackable_servant.hpp
@@ -73,23 +73,37 @@ public:
         , m_scheduler(boost::asynchronous::get_thread_scheduler<JOB>())
         , m_worker(w)
     {}
+
     // copy-ctor and operator= are needed for correct tracking
+    /*!
+     * \brief Copy-Constructor. Does not throw
+     */
     trackable_servant(trackable_servant const& rhs) noexcept
         : m_tracking(boost::make_shared<boost::asynchronous::track>())
         , m_scheduler(rhs.m_scheduler)
         , m_worker(rhs.m_worker)
     {
     }
+
+    /*!
+     * \brief Move-Constructor. Does not throw
+     */
     trackable_servant(trackable_servant&& rhs) noexcept
         : m_tracking(boost::make_shared<boost::asynchronous::track>())
         , m_scheduler(std::move(rhs.m_scheduler))
         , m_worker(std::move(rhs.m_worker))
     {
     }
+    /*!
+     * \brief Destructor.
+     */
     ~trackable_servant()
     {
     }
 
+    /*!
+     * \brief Assignment operator. Does not throw
+     */
     trackable_servant& operator= (trackable_servant const& rhs) noexcept
     {
         if (this != &rhs)
@@ -100,6 +114,10 @@ public:
         }
         return *this;
     }
+
+    /*!
+     * \brief Move assignment operator. Does not throw
+     */
     trackable_servant& operator= (trackable_servant&& rhs) noexcept
     {
         if (this != &rhs)
@@ -111,8 +129,13 @@ public:
         return *this;
     }
 
-    // make a callback, which posts if not the correct thread, and call directly otherwise
-    // in any case, check if this object is still alive
+    /*!
+     * \brief Makes a callback, which posts if not the correct thread, and calls directly otherwise
+     * \brief In any case, it will check if this object is still alive.
+     * \param func a functor will will safely be executed
+     * \param task_name which will be displayed in the diagnostic of the servant's scheduler.
+     * \param prio The priority of the functor within the servant's scheduler.
+     */
     template<class T>
     auto make_safe_callback(T func,
 #ifdef BOOST_ASYNCHRONOUS_REQUIRE_ALL_ARGUMENTS
@@ -125,14 +148,23 @@ public:
         return this->make_safe_callback_helper(boost::asynchronous::make_function(std::move(func)),task_name,prio);
     }
 
-    // returns a functor checking if servant is still alive
+    /*!
+     * \brief Returns a functor checking if servant is still alive
+     */
     std::function<bool()> make_check_alive_functor()const
     {
         boost::weak_ptr<track> tracking (m_tracking);
         return [tracking](){return !tracking.expired();};
     }
 
-    // helpers to make it easier using a timer service
+    /*!
+     * \brief Makes it easier to use a timer service
+     * \param t usually an asio_deadline_timer_proxy, which hides an asio timer behind an asynchronous proxy.
+     * \param func timer callback functor
+     * \param task_name which will be displayed in the diagnostic of the servant's scheduler.
+     * \param post_prio The priority of the timer call within the timer's scheduler.
+     * \param cb_prio The priority of the timer callback functor within the servant's scheduler.
+     */
     template <class Timer, class F>
 #ifdef BOOST_ASYNCHRONOUS_REQUIRE_ALL_ARGUMENTS
     void async_wait(Timer& t, F func, std::string const& task_name,std::size_t post_prio, std::size_t cb_prio)const
@@ -148,6 +180,16 @@ public:
                       task_name, post_prio, cb_prio
                       );
     }
+
+    /*!
+     * \brief Makes it easier to use a timer service
+     * \param t usually an asio_deadline_timer_proxy, which hides an asio timer behind an asynchronous proxy.
+     * \param timer_duration the timer value
+     * \param func timer callback functor
+     * \param task_name which will be displayed in the diagnostic of the servant's scheduler.
+     * \param post_prio The priority of the timer call within the timer's scheduler.
+     * \param cb_prio The priority of the timer callback functor within the servant's scheduler.
+     */
     template <class Timer, class Duration, class F>
 #ifdef BOOST_ASYNCHRONOUS_REQUIRE_ALL_ARGUMENTS
     void async_wait_duration(Timer& t, Duration timer_duration, F func, std::string const& task_name,std::size_t post_prio, std::size_t cb_prio)const
@@ -164,6 +206,14 @@ public:
                       );
     }
 
+    /*!
+     * \brief Posts a task to the servant's threadpool scheduler and get a safe callback when the task is done.
+     * \param func task functor
+     * \param cb_func callback functor
+     * \param task_name which will be displayed in the diagnostic of the servant's scheduler.
+     * \param post_prio The priority of the posted task within the threadpool scheduler.
+     * \param cb_prio The priority of the timer callback functor within the servant's scheduler.
+     */
     template <class F1, class F2>
 #ifdef BOOST_ASYNCHRONOUS_REQUIRE_ALL_ARGUMENTS
     void post_callback(F1 func,F2 cb_func, std::string const& task_name, std::size_t post_prio, std::size_t cb_prio) const
@@ -180,6 +230,17 @@ public:
                                         post_prio,
                                         cb_prio);
     }
+
+    /*!
+     * \brief Posts an interruptible task to the servant's threadpool scheduler and get a safe callback when the task is done.
+     * \brief The task must be interruptible as defined by Boost Thread
+     * \param func task functor
+     * \param cb_func callback functor
+     * \param task_name which will be displayed in the diagnostic of the servant's scheduler.
+     * \param post_prio The priority of the posted task within the threadpool scheduler.
+     * \param cb_prio The priority of the timer callback functor within the servant's scheduler.
+     * \return any_interruptible which can be used to interrupt the task
+     */
     template <class F1, class F2>
 #ifdef BOOST_ASYNCHRONOUS_REQUIRE_ALL_ARGUMENTS
     boost::asynchronous::any_interruptible interruptible_post_callback(F1 func,F2 cb_func, std::string const& task_name,
@@ -198,6 +259,15 @@ public:
                                         post_prio,
                                         cb_prio);
     }
+
+    /*!
+     * \brief Posts a task to any threadpool scheduler and get a safe callback when the task is done.
+     * \param func task functor
+     * \param cb_func callback functor
+     * \param task_name which will be displayed in the diagnostic of the servant's scheduler.
+     * \param post_prio The priority of the posted task within the threadpool scheduler.
+     * \param cb_prio The priority of the timer callback functor within the servant's scheduler.
+     */
     template <class Worker,class F1, class F2>
 #ifdef BOOST_ASYNCHRONOUS_REQUIRE_ALL_ARGUMENTS
     void post_callback(Worker& wscheduler,F1 func,F2 cb_func, std::string const& task_name, std::size_t post_prio, std::size_t cb_prio) const
@@ -214,6 +284,17 @@ public:
                                         post_prio,
                                         cb_prio);
     }
+
+    /*!
+     * \brief Posts an interruptible task to any threadpool scheduler and get a safe callback when the task is done.
+     * \brief The task must be interruptible as defined by Boost Thread
+     * \param func task functor
+     * \param cb_func callback functor
+     * \param task_name which will be displayed in the diagnostic of the servant's scheduler.
+     * \param post_prio The priority of the posted task within the threadpool scheduler.
+     * \param cb_prio The priority of the timer callback functor within the servant's scheduler.
+     * \return any_interruptible which can be used to interrupt the task
+     */
     template <class Worker,class F1, class F2>
 #ifdef BOOST_ASYNCHRONOUS_REQUIRE_ALL_ARGUMENTS
     boost::asynchronous::any_interruptible interruptible_post_callback(Worker& wscheduler,F1 func,F2 cb_func, std::string const& task_name,
@@ -232,6 +313,13 @@ public:
                                         post_prio,
                                         cb_prio);
     }
+
+    /*!
+     * \brief Posts a task to the servant's own scheduler.
+     * \param func task functor
+     * \param task_name which will be displayed in the diagnostic of the servant's scheduler.
+     * \param post_prio The priority of the posted task within the threadpool scheduler.
+     */
     template <class F1>
 #ifdef BOOST_ASYNCHRONOUS_REQUIRE_ALL_ARGUMENTS
     auto post_self(F1 func, std::string const& task_name, std::size_t post_prio)const
@@ -254,6 +342,14 @@ public:
         p.set_exception(boost::asynchronous::task_aborted_exception());
         return p.get_future();
     }
+
+    /*!
+     * \brief Posts an interruptible task to the servant's own scheduler.
+     * \param func task functor
+     * \param task_name which will be displayed in the diagnostic of the servant's scheduler.
+     * \param post_prio The priority of the posted task within the threadpool scheduler.
+     * \return any_interruptible which can be used to interrupt the task
+     */
     template <class F1>
 #ifdef BOOST_ASYNCHRONOUS_REQUIRE_ALL_ARGUMENTS
     auto interruptible_post_self(F1 func, std::string const& task_name,std::size_t post_prio)const
@@ -278,6 +374,13 @@ public:
         p.set_exception(boost::asynchronous::task_aborted_exception());
         return std::make_tuple(p.get_future(),boost::asynchronous::any_interruptible());
     }
+    /*!
+     * \brief Posts a task to the servant's threadpool scheduler and get a future corresponding to this task.
+     * \param func task functor
+     * \param task_name which will be displayed in the diagnostic of the servant's scheduler.
+     * \param post_prio The priority of the posted task within the threadpool scheduler.
+     * \return future<task result type>
+     */
     template <class F1>
 #ifdef BOOST_ASYNCHRONOUS_REQUIRE_ALL_ARGUMENTS
     auto post_future(F1 func, std::string const& task_name, std::size_t post_prio)const
@@ -293,6 +396,14 @@ public:
                                         task_name,
                                         post_prio);
     }
+
+    /*!
+     * \brief Posts an interruptible task to the servant's threadpool scheduler and get a future corresponding to this task.
+     * \param func task functor
+     * \param task_name which will be displayed in the diagnostic of the servant's scheduler.
+     * \param post_prio The priority of the posted task within the threadpool scheduler.
+     * \return tuple<future<task result type>,any_interruptible>
+     */
     template <class F1>
 #ifdef BOOST_ASYNCHRONOUS_REQUIRE_ALL_ARGUMENTS
     auto interruptible_post_future(F1 func, std::string const& task_name,std::size_t post_prio)const
@@ -310,6 +421,14 @@ public:
                                         task_name,
                                         post_prio);
     }
+
+    /*!
+     * \brief Posts a task to any threadpool scheduler and get a future corresponding to this task.
+     * \param func task functor
+     * \param task_name which will be displayed in the diagnostic of the servant's scheduler.
+     * \param post_prio The priority of the posted task within the threadpool scheduler.
+     * \return future<task result type>
+     */
     template <class Worker,class F1>
 #ifdef BOOST_ASYNCHRONOUS_REQUIRE_ALL_ARGUMENTS
     auto post_future(Worker& wscheduler,F1 func, std::string const& task_name, std::size_t post_prio)const
@@ -325,6 +444,14 @@ public:
                                         task_name,
                                         post_prio);
     }
+
+    /*!
+     * \brief Posts an interruptible task to any threadpool scheduler and get a future corresponding to this task.
+     * \param func task functor
+     * \param task_name which will be displayed in the diagnostic of the servant's scheduler.
+     * \param post_prio The priority of the posted task within the threadpool scheduler.
+     * \return tuple<future<task result type>,any_interruptible>
+     */
     template <class Worker,class F1>
 #ifdef BOOST_ASYNCHRONOUS_REQUIRE_ALL_ARGUMENTS
     auto interruptible_post_future(Worker& wscheduler,F1 func, std::string const& task_name,std::size_t post_prio)const
@@ -341,6 +468,16 @@ public:
                                         task_name,
                                         post_prio);
     }
+    /*!
+     * \brief Posts a task to any scheduler and get a callback corresponding to this task.
+     * \brief This is to be used with BOOST_ASYNC_UNSAFE_MEMBER provided by a Servant Proxy.
+     * \brief Unsafe means that the call to this Servant Proxy member must be protected through call_callback.
+     * \param s Scheduler where func executes
+     * \param func task functor
+     * \param task_name which will be displayed in the diagnostic of the servant's scheduler.
+     * \param post_prio The priority of the posted task within the threadpool scheduler.
+     * \return void
+     */
     template <class CallerSched,class F1, class F2>
 #ifdef BOOST_ASYNCHRONOUS_REQUIRE_ALL_ARGUMENTS
     void call_callback(CallerSched s, F1 func,F2 cb_func, std::string const& task_name, std::size_t post_prio, std::size_t cb_prio)const
@@ -357,6 +494,17 @@ public:
                                         post_prio,
                                         cb_prio);
     }
+
+    /*!
+     * \brief Posts an interruptible task to any scheduler and get a callback corresponding to this task.
+     * \brief This is to be used with BOOST_ASYNC_UNSAFE_MEMBER provided by a Servant Proxy.
+     * \brief Unsafe means that the call to this Servant Proxy member must be protected through call_callback.
+     * \param s Scheduler where func executes
+     * \param func task functor
+     * \param task_name which will be displayed in the diagnostic of the servant's scheduler.
+     * \param post_prio The priority of the posted task within the threadpool scheduler.
+     * \return boost::asynchronous::any_interruptible to be used for interruption
+     */
     template <class CallerSched,class F1, class F2>
 #ifdef BOOST_ASYNCHRONOUS_REQUIRE_ALL_ARGUMENTS
     boost::asynchronous::any_interruptible interruptible_call_callback(CallerSched s, F1 func,F2 cb_func, std::string const& task_name,
@@ -388,19 +536,38 @@ public:
         signal.connect(typename Signal::slot_type(make_safe_callback(std::move(slot),task_name,prio)).track(m_tracking));
     }
 
-
+    /*!
+     * \brief Returns the worker threadpool used by this servant.
+     * \return any_shared_scheduler_proxy<WJOB> hiding the threadpool type. WJOB is the Job type of the threadpool.
+     */
     boost::asynchronous::any_shared_scheduler_proxy<WJOB> const& get_worker()const
     {
         return m_worker;
     }
+
+    /*!
+     * \brief Replaces the worker threadpool used by this servant.
+     * \param w any_shared_scheduler_proxy<WJOB> hiding the threadpool type. WJOB is the Job type of the threadpool.
+     */
     void set_worker(boost::asynchronous::any_shared_scheduler_proxy<WJOB> w)
     {
         m_worker=w;
     }
+
+    /*!
+     * \brief Sets the worker threadpool used by this servant.
+     * \brief Warning. This is not made to replace the scheduler, which would be a race, but to set it in some corner cases.
+     * \param s boost::asynchronous::any_weak_scheduler<JOB> hiding the scheduler type. JOB is the Job type of the servant.
+     */
     void set_scheduler(boost::asynchronous::any_weak_scheduler<JOB> s)
     {
         m_scheduler=s;
     }
+
+    /*!
+     * \brief Returns the servant scheduler as a weak scheduler.
+     * \return boost::asynchronous::any_weak_scheduler<JOB>. JOB is the Job type of the servant scheduler.
+     */
     boost::asynchronous::any_weak_scheduler<JOB> const& get_scheduler()const
     {
         return m_scheduler;
