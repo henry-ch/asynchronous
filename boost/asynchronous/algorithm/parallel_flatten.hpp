a6c101c506ad988ed8c998c1460bd3879600d8bd
Tobias Holl <tobias.holl@tum.de>
2018-03-04 12:23:26 +0100

    Split off container_traits from parallel_flatten

--- boost/asynchronous/algorithm/parallel_flatten.hpp
+++ boost/asynchronous/algorithm/parallel_flatten.hpp
@@ -19,6 +19,7 @@
 #include <boost/asynchronous/algorithm/parallel_for.hpp>
 #include <boost/asynchronous/algorithm/parallel_transform_exclusive_scan.hpp>
 #include <boost/asynchronous/algorithm/then.hpp>
+#include <boost/asynchronous/detail/container_traits.hpp>
 #include <boost/asynchronous/detail/metafunctions.hpp>
 
 // Unfortunately, Boost.Range leads to some pretty ugly ambiguities with std::begin/std::end if we rely on ADL.
@@ -29,65 +30,6 @@ namespace boost
 {
 namespace asynchronous
 {
-
-namespace detail
-{
-    // Helper namespace to avoid polluting boost::asynchronous::detail
-    namespace flatten_helper
-    {
-        struct invalid {};
-
-        // Iterators
-
-        template <typename Container, typename Iterator = decltype(std::begin(std::declval<Container>()))>
-        Iterator begin_detector(const Container&);
-        invalid  begin_detector(...);
-
-        template <typename Container> struct iterator_type_detector { using type = decltype(begin_detector(std::declval<Container>())); };
-
-        // Size
-
-#if __cpp_lib_nonmember_container_access
-        template <typename Container, typename Size = decltype(std::size(std::declval<Container>()))>
-        Size    size_detector(const Container&);
-        invalid size_detector(...);
-#else
-        template <typename Container, typename Size = decltype(std::distance(std::declval<typename iterator_type_detector<Container>::type>(), std::declval<typename iterator_type_detector<Container>::type>()))>
-        Size    size_detector(const Container&);
-        invalid size_detector(...);
-#endif
-
-        template <typename Container> struct size_type_detector { using type = decltype(size_detector(std::declval<Container>())); };
-
-        // Values
-
-        template <typename Container, typename Value = typename std::iterator_traits<typename iterator_type_detector<Container>::type>::value_type>
-        Value   value_detector(const Container&);
-        invalid value_detector(...);
-
-        template <typename Container> struct value_type_detector { using type = decltype(value_detector(std::declval<Container>())); };
-    }
-
-    // Container traits
-    template <class Container>
-    struct container_traits
-    {
-        using iterator_type = typename flatten_helper::iterator_type_detector<Container>::type;
-        using size_type     = typename flatten_helper::size_type_detector<Container>::type;
-        using value_type    = typename flatten_helper::value_type_detector<Container>::type;
-    };
-
-    // Size of the container
-    template <class Container>
-    typename boost::asynchronous::detail::container_traits<Container>::size_type container_size(const Container& c)
-    {
-#if __cpp_lib_nonmember_container_access
-        return std::size(c);
-#else
-        return std::distance(std::begin(c), std::end(c));
-#endif
-    }
-} // namespace detail
 
 // Flattens a container of containers into a single container
 // By default, returns an std::vector<T>, where T is the type contained in the inner vectors.
