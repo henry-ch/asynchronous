1f0acf7bcd628d3ad0db50d8b921d5494f43f8a1
Alexander Shevchenko <mtrx86@gmail.com>
2018-10-29 16:06:26 +0100

    added stop event for parallel_all_of_helper

--- boost/asynchronous/algorithm/parallel_any_of.hpp
+++ boost/asynchronous/algorithm/parallel_any_of.hpp
@@ -46,9 +46,13 @@ struct any_of_operator
     {
         return std::any_of(beg,end,f);
     }
-    bool merge(bool b1, bool b2)const
+    constexpr bool merge(bool b1, bool b2)const
     {
         return b1 || b2;
+    }
+    constexpr bool cancel_value()
+    {
+        return merge(true, false);
     }
 };
 }
@@ -65,7 +69,7 @@ parallel_any_of(Iterator beg, Iterator end,Func func,long cutoff,
 {
     return boost::asynchronous::top_level_callback_continuation_job<bool,Job>
             (boost::asynchronous::detail::parallel_all_of_helper<Iterator,Func,boost::asynchronous::detail::any_of_operator,Job>
-                (beg,end,func,cutoff,task_name,prio));
+                (beg,end,func,cutoff,std::make_shared<std::atomic_bool>(false),task_name,prio));
 }
 
 // version for ranges returned as continuations
